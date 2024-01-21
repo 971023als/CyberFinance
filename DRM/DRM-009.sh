d6113f0a06ee3ed45c1db71605a8b015da2310f3
971023als <971023als@naver.com>
2024-01-21 22:04:30 +0900

    Update DRM-009.sh

--- DRM/DRM-009.sh
+++ DRM/DRM-009.sh
@@ -2,32 +2,40 @@
 
 . function.sh
 
-TMP1=$(SCRIPTNAME).log
-> $TMP1
+TMP1=$(mktemp /tmp/$(basename $0).XXXXXX)
 
-BAR 
+BAR
 
-CODE [SRV-009] SMTP 서비스 스팸 메일 릴레이 제한 미설정
+CODE [DBM-009] 사용되지 않는 세션 종료 미흡
 
 cat << EOF >> $result
-[양호]: SMTP 서비스에서 스팸 메일 릴레이를 제한하는 설정이 구성된 경우
-[취약]: SMTP 서비스에서 스팸 메일 릴레이를 제한하는 설정이 구성되지 않은 경우
+[양호]: 사용되지 않는 데이터베이스 세션의 종료 시간이 적절히 설정되어 있는 경우
+[취약]: 사용되지 않는 데이터베이스 세션의 종료 시간이 설정되어 있지 않은 경우
 EOF
 
 BAR
 
-# Postfix의 main.cf 설정 파일을 확인합니다.
-POSTFIX_MAIN_CF="/etc/postfix/main.cf"
-RELAY_RESTRICTION_SETTING="mynetworks"
+read -p "Enter MySQL username: " MYSQL_USER
+read -sp "Enter MySQL password: " MYSQL_PASS
+echo
 
-# 릴레이 제한 설정을 확인합니다.
-if grep -qE "^$RELAY_RESTRICTION_SETTING\s*=" "$POSTFIX_MAIN_CF"; then
-    networks=$(grep -E "^$RELAY_RESTRICTION_SETTING\s*=" "$POSTFIX_MAIN_CF" | cut -d'=' -f2)
-    OK "릴레이 제한이 설정되어 있습니다: $networks"
+MYSQL_CMD="mysql -u $MYSQL_USER -p$MYSQL_PASS -Bse"
+
+SESSION_TIMEOUT=$($MYSQL_CMD "SHOW VARIABLES LIKE 'wait_timeout';")
+
+if [[ -z "$SESSION_TIMEOUT" ]]; then
+    WARN "세션 종료 시간이 설정되어 있지 않습니다."
 else
-    WARN "릴레이 제한 설정이 '$POSTFIX_MAIN_CF' 파일에 구성되어 있지 않습니다."
+    TIMEOUT_VALUE=$(echo $SESSION_TIMEOUT | awk '{print $2}')
+    if [[ "$TIMEOUT_VALUE" -le 300 ]]; then
+        OK "세션 종료 시간이 적절히 설정되어 있습니다: $TIMEOUT_VALUE seconds."
+    else
+        WARN "세션 종료 시간이 너무 길게 설정되어 있습니다: $TIMEOUT_VALUE seconds."
+    fi
 fi
 
 cat $result
 
-echo ; echo
+rm $TMP1
+
+echo
