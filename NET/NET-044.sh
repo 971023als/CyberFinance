3899cbe01ffe2340fd12cf1e1ede635a1980c717
971023als <971023als@naver.com>
2024-01-23 16:06:01 +0900

    44

--- NET/NET-044.sh
+++ NET/NET-044.sh
@@ -1,30 +1,40 @@
 #!/bin/bash
 
+# Function library inclusion
 . function.sh
 
-TMP1=$(SCRIPTNAME).log
-> $TMP1
+# Temporary log file creation
+TMP1=$(mktemp)
+> "$TMP1"
 
+# Start delimiter
 BAR
+CODE [NET-044] ICMP Unreachable 차단 미설정
 
-CODE [SRV-026] root 계정 원격 접속 제한 미비
-
-cat << EOF >> $result
-[양호]: SSH를 통한 root 계정의 원격 접속이 제한된 경우
-[취약]: SSH를 통한 root 계정의 원격 접속이 제한되지 않은 경우
+cat << EOF >> "$result"
+[양호]: 네트워크 장비에 ICMP Unreachable 차단이 적절하게 설정된 경우
+[취약]: 네트워크 장비에 ICMP Unreachable 차단이 설정되지 않은 경우
 EOF
 
+# End delimiter
 BAR
 
-# SSH 설정 파일에서 root 로그인을 허용하는 설정을 확인합니다.
-SSH_CONFIG_FILE="/etc/ssh/sshd_config"
+# Network devices list
+DEVICES=("Device1" "Device2" "Device3") # Replace with actual device list
 
-if grep -q "^PermitRootLogin no" "$SSH_CONFIG_FILE"; then
-    OK "SSH를 통한 root 계정의 원격 접속이 제한됩니다."
-else
-    WARN "SSH를 통한 root 계정의 원격 접속이 허용됩니다."
-fi
+# Checking ICMP unreachable blocking on each device
+for device in "${DEVICES[@]}"; do
+    # Connect to each device and check ICMP unreachable blocking configuration
+    ICMP_UNREACHABLE_BLOCKING=$(ssh $device "show running-config | include icmp-unreachable-block") # Replace with the actual command for checking ICMP unreachable
 
-cat $result
+    # Checking the status of ICMP unreachable blocking
+    if [[ $ICMP_UNREACHABLE_BLOCKING ]]; then
+        OK "$device 에서 ICMP Unreachable 차단이 적절하게 설정되었습니다."
+    else
+        WARN "$device 에서 ICMP Unreachable 차단이 설정되지 않았습니다."
+    fi
+done
+
+cat "$result"
 
 echo ; echo
