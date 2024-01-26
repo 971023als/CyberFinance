5916ef4d5768d87748701abc19f639f493f0b1c4
971023als <971023als@naver.com>
2024-01-26 18:47:21 +0900

    1번 수정 완로

--- MOB/MOB-SER-001.sh
+++ MOB/MOB-SER-001.sh
@@ -1,14 +1,54 @@
 Java.perform(function () {
-    // 데이터베이스 쿼리를 실행하는 클래스와 메소드 후킹
     var SQLiteDatabase = Java.use("android.database.sqlite.SQLiteDatabase");
     var rawQuery = SQLiteDatabase.rawQuery.overload('java.lang.String', '[Ljava.lang.String;');
 
-    // rawQuery 메소드 후킹하여 쿼리 내용 로그 기록
     rawQuery.implementation = function (sql, selectionArgs) {
         console.log("실행되는 SQL 쿼리: " + sql);
-        if (selectionArgs !== null) {
-            console.log("쿼리 파라미터: " + Java.array('java.lang.String', selectionArgs).toString());
+
+        // 잠재적인 SQL 인젝션 패턴 검사 및 실행 시간 분석
+        detectSQLInjection(sql, selectionArgs);
+
+        var startTime = Date.now();
+        var result = rawQuery.call(this, sql, selectionArgs);
+        var executionTime = Date.now() - startTime;
+
+        if (executionTime > 1000) { // 1초 이상 걸리는 쿼리에 대한 경고
+            console.log("경고: 비정상적으로 긴 실행 시간 (" + executionTime + "ms)");
         }
-        return rawQuery.call(this, sql, selectionArgs);
+
+        return result;
     };
+
+    function detectSQLInjection(sql, args) {
+        var suspiciousPatterns = [
+            'union', 'select', '--', ';', '\\', '/', '/*', '*/', '@@', '@', 'char', 'nchar', 'varchar', 'nvarchar', 
+            'alter', 'begin', 'cast', 'create', 'cursor', 'declare', 'delete', 'drop', 'end', 'exec', 'execute', 
+            'fetch', 'insert', 'kill', 'open', 'sys', 'sysobjects', 'syscolumns', 'table', 'update'
+        ];
+        var lowerSql = sql.toLowerCase();
+        
+        // SQL 쿼리 문자열 및 파라미터에서 의심스러운 패턴 검사
+        suspiciousPatterns.forEach(function(pattern) {
+            if (lowerSql.includes(pattern)) {
+                console.log("경고: 잠재적인 SQL 인젝션 패턴 발견 (" + pattern + ")");
+            }
+        });
+
+        if (args !== null) {
+            for (var i = 0; i < args.length; i++) {
+                var arg = args[i].toLowerCase();
+                suspiciousPatterns.forEach(function(pattern) {
+                    if (arg.includes(pattern)) {
+                        console.log("경고: 쿼리 파라미터에서 잠재적인 SQL 인젝션 패턴 발견 (" + pattern + ")");
+                    }
+                });
+            }
+        }
+
+        // 특정 키워드의 사용 빈도수 분석
+        var keywordCount = (sql.match(/union|select|insert|delete|update/gi) || []).length;
+        if (keywordCount > 5) {
+            console.log("경고: 비정상적인 키워드 사용 빈도 (" + keywordCount + "회)");
+        }
+    }
 });
