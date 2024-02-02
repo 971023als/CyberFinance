10bcd78995cce9f26b5193bf76386efed9956c85
971023als <971023als@naver.com>
2024-02-02 09:43:24 +0900

    1 2번 수정

--- SRV/SRV-002.sh
+++ SRV/SRV-002.sh
@@ -7,7 +7,7 @@ TMP1=$(basename "$0").log
 
 BAR
 
-CODE [SRV-001] SNMP 서비스 Get Community 스트링 설정 오류
+CODE [SRV-002] SNMP 서비스 Set Community 스트링 설정 오류
 
 cat << EOF >> $TMP1
 
@@ -19,18 +19,18 @@ EOF
 
 BAR
 
-# Check if SNMP service is running
+# SNMP service running check
 ps_snmp_count=$(ps -ef | grep -i 'snmp' | grep -v 'grep' | wc -l)
 
 if [ $ps_snmp_count -gt 0 ]; then
-    # Check SNMP configuration file for Community string
-    snmpdconf_file="/etc/snmp/snmpd.conf" # Default path for Linux; might need adjustment for other OS
+    # Check SNMP configuration file for Set Community string
+    snmpdconf_file="/etc/snmp/snmpd.conf" # Default path for Linux; adjust for other OS as needed
     if [ -f "$snmpdconf_file" ]; then
-        # Check if "public" or "private" strings are present
+        # Check for "public" or "private" strings being used
         if grep -qiE 'public|private' $snmpdconf_file; then
-            WARN "기본 SNMP Community 스트링(public/private)이 사용됨" >> $TMP1
+            WARN "기본 SNMP Set Community 스트링(public/private)이 사용됨" >> $TMP1
         else
-            OK "기본 SNMP Community 스트링(public/private)이 사용되지 않음" >> $TMP1
+            OK "기본 SNMP Set Community 스트링(public/private)이 사용되지 않음" >> $TMP1
         fi
     else
         WARN "SNMP 구성 파일($snmpdconf_file)을 찾을 수 없음" >> $TMP1
@@ -40,4 +40,6 @@ else
 fi
 
 cat $TMP1
+
 echo ; echo
+
