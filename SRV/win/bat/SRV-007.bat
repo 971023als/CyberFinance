42e7e4e48938bdc6ad046036aebdf3c2a66afc24
971023als <971023als@naver.com>
2024-02-11 21:18:58 +0900

    Update SRV-007.bat

--- SRV/win/bat/SRV-007.bat
+++ SRV/win/bat/SRV-007.bat
@@ -1,99 +1,100 @@
-rem windows server script edit 2020
 @echo off
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 if '%errorlevel%' NEQ '0' (
-    echo °ü¸®ÀÚ ±ÇÇÑÀ» ¿äÃ»ÇÕ´Ï´Ù...
+    echo ê´€ë¦¬ìž ê¶Œí•œì„ ìš”ì²­í•©ë‹ˆë‹¤...
     goto UACPrompt
 ) else ( goto gotAdmin )
+
 :UACPrompt
     echo Set UAC = CreateObject^("Shell.Application"^) > "%getadmin.vbs"
     set params = %*:"=""
     echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "getadmin.vbs"
     "getadmin.vbs"
-	del "getadmin.vbs"
+    del "getadmin.vbs"
     exit /B
 
 :gotAdmin
 chcp 437
 color 02
 setlocal enabledelayedexpansion
-echo ------------------------------------------Setting---------------------------------------
+echo ------------------------------------------ì„¤ì • ì‹œìž‘---------------------------------------
 rd /S /Q C:\Window_%COMPUTERNAME%_raw
 rd /S /Q C:\Window_%COMPUTERNAME%_result
 mkdir C:\Window_%COMPUTERNAME%_raw
 mkdir C:\Window_%COMPUTERNAME%_result
 del C:\Window_%COMPUTERNAME%_result\W-Window-*.txt
 secedit /EXPORT /CFG C:\Window_%COMPUTERNAME%_raw\Local_Security_Policy.txt
-fsutil file createnew C:\Window_%COMPUTERNAME%_raw\compare.txt  0
+fsutil file createnew C:\Window_%COMPUTERNAME%_raw\compare.txt 0
 cd >> C:\Window_%COMPUTERNAME%_raw\install_path.txt
-for /f "tokens=2 delims=:" %%y in ('type C:\Window_%COMPUTERNAME%_raw\install_path.txt') do set install_path=c:%%y 
+for /f "tokens=2 delims=:" %%y in ('type C:\Window_%COMPUTERNAME%_raw\install_path.txt') do set install_path=c:%%y
 systeminfo >> C:\Window_%COMPUTERNAME%_raw\systeminfo.txt
-echo ------------------------------------------IIS Setting-----------------------------------
+echo ------------------------------------------IIS ì„¤ì • í™•ì¸-----------------------------------
 type %WinDir%\System32\Inetsrv\Config\applicationHost.Config >> C:\Window_%COMPUTERNAME%_raw\iis_setting.txt
 type C:\Window_%COMPUTERNAME%_raw\iis_setting.txt | findstr "physicalPath bindingInformation" >> C:\Window_%COMPUTERNAME%_raw\iis_path1.txt
 set "line="
 for /F "delims=" %%a in ('type C:\Window_%COMPUTERNAME%_raw\iis_path1.txt') do (
-set "line=!line!%%a" 
+set "line=!line!%%a"
 )
 echo !line!>>C:\Window_%COMPUTERNAME%_raw\line.txt
 for /F "tokens=1 delims=*" %%a in ('type C:\Window_%COMPUTERNAME%_raw\line.txt') do (
-	echo %%a >> C:\Window_%COMPUTERNAME%_raw\path1.txt
+    echo %%a >> C:\Window_%COMPUTERNAME%_raw\path1.txt
 )
 for /F "tokens=2 delims=*" %%a in ('type C:\Window_%COMPUTERNAME%_raw\line.txt') do (
-	echo %%a >> C:\Window_%COMPUTERNAME%_raw\path2.txt
+    echo %%a >> C:\Window_%COMPUTERNAME%_raw\path2.txt
 )
 for /F "tokens=3 delims=*" %%a in ('type C:\Window_%COMPUTERNAME%_raw\line.txt') do (
-	echo %%a >> C:\Window_%COMPUTERNAME%_raw\path3.txt
+    echo %%a >> C:\Window_%COMPUTERNAME%_raw\path3.txt
 )
 for /F "tokens=4 delims=*" %%a in ('type C:\Window_%COMPUTERNAME%_raw\line.txt') do (
-	echo %%a >> C:\Window_%COMPUTERNAME%_raw\path4.txt
+    echo %%a >> C:\Window_%COMPUTERNAME%_raw\path4.txt
 )
 for /F "tokens=5 delims=*" %%a in ('type C:\Window_%COMPUTERNAME%_raw\line.txt') do (
-	echo %%a >> C:\Window_%COMPUTERNAME%_raw\path5.txt
+    echo %%a >> C:\Window_%COMPUTERNAME%_raw\path5.txt
 )
 type C:\WINDOWS\system32\inetsrv\MetaBase.xml >> C:\Window_%COMPUTERNAME%_raw\iis_setting.txt
-echo ------------------------------------------end-------------------------------------------
+echo ------------------------------------------ì„¤ì • ì™„ë£Œ-------------------------------------------
+
 echo ------------------------------------------SRV-001------------------------------------------
-SRV-001 (Windows) SNMP Community ½ºÆ®¸µ ¼³Á¤ ¹ÌÈí
+SRV-001 (Windows) SNMP Community ë¬¸ìžì—´ ì•ˆì „ ì„¤ì •
 
-¡¼ »ó¼¼¼³¸í ¡½
-SNMP ¼­ºñ½º´Â ³×Æ®¿öÅ© °ü¸® ¹× ³×Æ®¿öÅ© ÀåÄ¡ÀÇ µ¿ÀÛÀ» °¨½Ã/ÅëÇÒÇÏ´Â SNMP ÇÁ·ÎÅäÄÝÀ» ±â¹ÝÀ¸·Î ÇÏ´Â ¼­ºñ½º·Î, SNMP Åë½Å ½Ã Á¢±Ù Çã¿ë ¿©ºÎ¸¦ °áÁ¤ÇÏ±â À§ÇÑ SNMP community stringÀÇ º¹Àâµµ°¡ ³·°Ô ¼³Á¤µÇ¾ú´ÂÁö Á¡°Ë
+ìƒì„¸ ì„¤ëª… ë° ì ê²€ ë°©ë²•
+SNMP ì„œë¹„ìŠ¤ëŠ” ë„¤íŠ¸ì›Œí¬ ê´€ë¦¬ ë° ë„¤íŠ¸ì›Œí¬ ìœ„ì¹˜ ì •ë³´ë¥¼ ìˆ˜ì§‘/ê´€ë¦¬í•˜ëŠ” SNMP í”„ë¡œí† ì½œì„ ì§€ì›í•˜ëŠ” ì• í”Œë¦¬ì¼€ì´ì…˜ìœ¼ë¡œ, SNMP ì„œë¹„ìŠ¤ë¥¼ í†µí•´ ì™¸ë¶€ í˜¸ìŠ¤íŠ¸ê°€ ë„¤íŠ¸ì›Œí¬ ì •ë³´ë¥¼ ìˆ˜ì§‘í•  ìˆ˜ ìžˆìœ¼ë¯€ë¡œ SNMP community stringì´ ì•ˆì „í•˜ê²Œ ì„¤ì •ë˜ì–´ì•¼ í•©ë‹ˆë‹¤.
 
-¡¼ ÆÇ´Ü±âÁØ ¡½
-- ¾çÈ£ : SNMP Community String ÃÊ±â °ª(Public, Private)ÀÌ ¾Æ´Ï°í, ¾Æ·¡ÀÇ º¹Àâµµ¸¦ ¸¸Á· ÇÒ °æ¿ì
-- Ãë¾à : SNMP Community String ÃÊ±â °ª(Public, Private)ÀÌ°Å³ª, º¹Àâµµ¸¦ ¸¸Á·ÇÏÁö ¾ÊÀ» °æ¿ì
+ì ê²€ ê¸°ì¤€ ë° ê²°ê³¼
+- í•©ê²©: SNMP Community Stringì´ ê¸°ë³¸ê°’(Public, Private)ì´ ì•„ë‹ˆë©°, ì•„ëž˜ ê¸°ì¤€ì— ë¶€í•©í•˜ëŠ” ê²½ìš°
+- ë¶ˆí•©ê²©: SNMP Community Stringì´ ê¸°ë³¸ê°’(Public, Private)ì´ê±°ë‚˜, ì•ˆì „í•˜ì§€ ì•Šì€ ì„¤ì •ì¸ ê²½ìš°
 
-¡Ø (º¹Àâµµ) ±âº»°ª(public, private) ¹Ì»ç¿ë, ¿µ¹®ÀÚ, ¼ýÀÚ°¡ Æ÷ÇÔ 10ÀÚ¸® ÀÌ»ó ¶Ç´Â ¿µ¹®ÀÚ, ¼ýÀÚ, Æ¯¼ö¹®ÀÚ Æ÷ÇÔ 8ÀÚ¸® ÀÌ»ó
-¡Ø SNMP v3ÀÇ °æ¿ì º°µµ ÀÎÁõ ±â´ÉÀ» »ç¿ëÇÏ°í, ÇØ´ç ºñ¹Ð¹øÈ£°¡ º¹Àâµµ¸¦ ¸¸Á·ÇÒ °æ¿ì "¾çÈ£"·Î ÆÇ´Ü
+(ì•ˆì „ ê¸°ì¤€) ê¸°ë³¸ê°’(public, private) ì´ì™¸, ì•ŒíŒŒë²³, ìˆ«ìžê°€ í¬í•¨ëœ 10ìž ì´ìƒ ë˜ëŠ” íŠ¹ìˆ˜ë¬¸ìž, ëŒ€ì†Œë¬¸ìžë¥¼ í¬í•¨í•œ 8ìž ì´ìƒ
+SNMP v3ì„ ì‚¬ìš©í•˜ì—¬ ë³´ì•ˆì„± ê°•í™” ë° í•´ë‹¹ ë¹„ë°€ë²ˆí˜¸ê°€ ì•ˆì „í•˜ê²Œ ì„¤ì •ëœ ê²½ìš° "í•©ê²©"ìœ¼ë¡œ íŒë‹¨
 
-¡¼ ÆÇ´Ü¹æ¹ý ¡½
-  1. SNMP ¼­ºñ½º ÀÇ SNMP Community String ÀÌ º¹Àâµµ¸¦ ¸¸Á·ÇÏ´ÂÁö È®ÀÎ
-      ¡Ø <registry_path> : HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\SNMP\Parameters\ValidCommunities
-      ¡Ø ·¹Áö½ºÆ®¸® °ªÀÌ Á¸ÀçÇÏÁö ¾ÊÀ¸¸é SNMP ¼­ºñ½º°¡ ½ÇÇà ÁßÀÌ¾îµµ ¼­ºñ½º¸¦ »ç¿ëÇÒ ¼ö ¾øÀ¸¹Ç·Î "¾çÈ£" ·Î ÆÇ´Ü
+ì ê²€ ë°©ë²• ë° ì„¤ì • ë°©ë²•
+  1. SNMP ì„œë¹„ìŠ¤ ë° SNMP Community Stringì˜ ì•ˆì „ì„± ì—¬ë¶€ë¥¼ í™•ì¸
+      ê²½ë¡œ: HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\SNMP\Parameters\ValidCommunities
+      ë„¤íŠ¸ì›Œí¬ìƒì—ì„œ SNMP ì„œë¹„ìŠ¤ê°€ í™œì„±í™”ë˜ì–´ ì™¸ë¶€ì—ì„œ SNMP í”„ë¡œí† ì½œì„ í†µí•´ ì •ë³´ë¥¼ ìˆ˜ì§‘í•  ìˆ˜ ìžˆëŠ” ê²½ìš° "ë¶ˆí•©ê²©"ìœ¼ë¡œ íŒë‹¨
 
-  ¡á Windows 2008, 2008 R2, 2012, 2012 R2, 2016, 2019, 2022
-      ¨ç ½ÃÀÛ > ½ÇÇà > services.msc > ¼­ºñ½º > SNMP Service ¼Ó¼º > "º¸¾È" ÅÇ >
-      ¨è "¹Þ¾ÆµéÀÎ Ä¿¹Â´ÏÆ¼ ÀÌ¸§"  "> SNMP community string" Ç×¸ñ
-  ¶Ç´Â
+  Windows 2008, 2008 R2, 2012, 2012 R2, 2016, 2019, 2022
+      ë°©ë²•: ì œì–´íŒ > ê´€ë¦¬ ë„êµ¬ > services.msc > ì†ì„± > SNMP Service ì†ì„± > "ë³´ì•ˆ" íƒ­ >
+      "ìˆ˜ì‹ í•˜ëŠ” ì»¤ë®¤ë‹ˆí‹° ì´ë¦„" > "SNMP community string" ìƒíƒœ
+  ë˜ëŠ”
       cmd > reg query <registry_path>
           <SNMP_community_string>     REG_DWORD    0x4
-  ¶Ç´Â
+  ë˜ëŠ”
       cmd > reg query <registry_path>
           ERROR: The system was unable to find the specified registry key or value.
 
-  ¡Ø "ValidCommunities" Key ÀÇ Value °ª
-      - ÀÌ¸§ : 
-      - Á¾·ù : REG_DWORD
-      - µ¥ÀÌÅÍ : 1(¾øÀ½), 2(¾Ë¸²), 4(ÀÐ±â Àü¿ë) 8(ÀÐ±â, ¾²±â), 16(ÀÐ±â, ¸¸µé±â)
+  "ValidCommunities" Keyì˜ Value ì„¤ëª…
+      - ì´ë¦„:
+      - ìœ í˜•: REG_DWORD
+      - ë°ì´í„°: 1(ì½ê¸°), 2(ì“°ê¸°), 4(ìƒì„± ì‚­ì œ) 8(ìƒì„±, ì‚­ì œ), 16(ìƒì„±, ì‚­ì œ, ì•Œë¦¼)
 
-¡¼ Á¶Ä¡¹æ¹ý ¡½
-  1. SNMP Community String À» º¹Àâµµ¸¦ ¸¸Á·ÇÏ´Â °ªÀ¸·Î ¼³Á¤
-      ¡Ø À¯ÃßÇÏ±â ½¬¿î ¹®ÀÚ¿­(±â°ü¸í, ±â±â¸í µî) »ç¿ë ÀÚÁ¦ ¡æ ÀÏºÎ ±â°ü¿¡¼­ ¼öÁ¤ ±Ç°íÇÏ´Â ±âÁØ
-      ¡Ø "¾²±â" ±ÇÇÑÀÌ ÇÊ¿äÇÑ °æ¿ì°¡ ¾Æ´Ï¸é "ÀÐ±â Àü¿ë" ±ÇÇÑ ºÎ¿©
-      ¡Ø NMS, ¸ð´ÏÅÍ¸µ µî ¸ñÀûÀ¸·Î SNMP ¼­ºñ½º¸¦ »ç¿ëÇÏ´Â °æ¿ì SNMP Manager ¿Í Agent ¿¡ °°Àº Community String ÀÌ ¼³Á¤µÇ¾î ÀÖ¾î¾ß ÀÎÁõ °¡´É
-  2. SNMP ¼­ºñ½º Àç½ÃÀÛ
-echo -------------------------------------------end------------------------------------------
+ì„¤ì • ë°©ë²•
+  1. SNMP Community Stringì˜ ì•ˆì „ì„±ì„ ê°•í™”í•˜ëŠ” ì„¤ì •ì„ ì ìš©
+      ì•ŒíŒŒë²³, ìˆ«ìžë¥¼ í¬í•¨í•œ ë³µìž¡í•œ ë¬¸ìžì—´ë¡œ ë³€ê²½í•˜ê³ , ì™¸ë¶€ì—ì„œ ì ‘ê·¼í•  ìˆ˜ ì—†ë„ë¡ ì„¤ì •
+      "ë³´ì•ˆ" íƒ­ì´ í•„ìš”í•œ ê²½ìš°ê°€ ì•„ë‹ˆë©´ "ìƒì„± ì‚­ì œ" ê¶Œí•œ ì œí•œ
+      NMS, ê´€ë¦¬ìžë§Œ SNMP ì„œë¹„ìŠ¤ë¥¼ í†µí•´ ì •ë³´ë¥¼ ìˆ˜ì§‘í•  ìˆ˜ ìžˆë„ë¡ SNMP Manager ë° Agentì— ë™ì¼í•œ Community Stringì´ ì„¤ì •ë˜ì–´ ìžˆì–´ì•¼ í•©ë‹ˆë‹¤.
+  2. SNMP ì„œë¹„ìŠ¤ ê°•í™”
+echo -------------------------------------------end-------------------------------------------
 
-echo --------------------------------------SRV-004 ºÒÇÊ¿äÇÑ SMTP ¼­ºñ½º ½ÇÇà ¿©ºÎ------------------------------------->> C:\Window_%COMPUTERNAME%_result\W-Window-%COMPUTERNAME%-rawdata.txt
+echo --------------------------------------SRV-004 í•„ìˆ˜ì ì¸ SMTP ì„œë¹„ìŠ¤ ì‹¤í–‰ ìƒíƒœ í™•ì¸------------------------------------->> C:\Window_%COMPUTERNAME%_result\W-Window-%COMPUTERNAME%-rawdata.txt
 sc query smtp >> C:\Window_%COMPUTERNAME%_result\W-Window-%COMPUTERNAME%-rawdata.txt
-echo ------------------------------------------------------------------------------->> C:\Window_%COMPUTERNAME%_result\W-Window-%COMPUTERNAME%-rawdata.txt
+echo -------------------------------------------------------------------------------->> C:\Window_%COMPUTERNAME%_result\W-Window-%COMPUTERNAME%-rawdata.txt
