95f317b97a1d8fd29b778eb3e5db7cc8fa4408a7
971023als <971023als@naver.com>
2024-02-12 20:20:37 +0900

    39 40

--- SRV/win/bat/SRV-040.bat
+++ SRV/win/bat/SRV-040.bat
@@ -1,8 +1,7 @@
-rem windows server script edit 2020
 @echo off
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 if '%errorlevel%' NEQ '0' (
-    echo °ü¸®ÀÚ ±ÇÇÑÀ» ¿äÃ»ÇÕ´Ï´Ù...
+    echo ê´€ë¦¬ìž ê¶Œí•œì´ í•„ìš”í•©ë‹ˆë‹¤...
     goto UACPrompt
 ) else ( goto gotAdmin )
 :UACPrompt
@@ -10,11 +9,11 @@ if '%errorlevel%' NEQ '0' (
     set params = %*:"=""
     echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "getadmin.vbs"
     "getadmin.vbs"
-	del "getadmin.vbs"
+    del "getadmin.vbs"
     exit /B
 
 :gotAdmin
-chcp 437
+chcp 949
 color 02
 setlocal enabledelayedexpansion
 echo ------------------------------------------Setting---------------------------------------
@@ -37,64 +36,38 @@ set "line=!line!%%a"
 )
 echo !line!>>C:\Window_%COMPUTERNAME%_raw\line.txt
 for /F "tokens=1 delims=*" %%a in ('type C:\Window_%COMPUTERNAME%_raw\line.txt') do (
-	echo %%a >> C:\Window_%COMPUTERNAME%_raw\path1.txt
+    echo %%a >> C:\Window_%COMPUTERNAME%_raw\path1.txt
 )
 for /F "tokens=2 delims=*" %%a in ('type C:\Window_%COMPUTERNAME%_raw\line.txt') do (
-	echo %%a >> C:\Window_%COMPUTERNAME%_raw\path2.txt
+    echo %%a >> C:\Window_%COMPUTERNAME%_raw\path2.txt
 )
 for /F "tokens=3 delims=*" %%a in ('type C:\Window_%COMPUTERNAME%_raw\line.txt') do (
-	echo %%a >> C:\Window_%COMPUTERNAME%_raw\path3.txt
+    echo %%a >> C:\Window_%COMPUTERNAME%_raw\path3.txt
 )
 for /F "tokens=4 delims=*" %%a in ('type C:\Window_%COMPUTERNAME%_raw\line.txt') do (
-	echo %%a >> C:\Window_%COMPUTERNAME%_raw\path4.txt
+    echo %%a >> C:\Window_%COMPUTERNAME%_raw\path4.txt
 )
 for /F "tokens=5 delims=*" %%a in ('type C:\Window_%COMPUTERNAME%_raw\line.txt') do (
-	echo %%a >> C:\Window_%COMPUTERNAME%_raw\path5.txt
+    echo %%a >> C:\Window_%COMPUTERNAME%_raw\path5.txt
 )
 type C:\WINDOWS\system32\inetsrv\MetaBase.xml >> C:\Window_%COMPUTERNAME%_raw\iis_setting.txt
 echo ------------------------------------------end-------------------------------------------
 echo ------------------------------------------SRV-001------------------------------------------
-SRV-001 (Windows) SNMP Community ½ºÆ®¸µ ¼³Á¤ ¹ÌÈí
-
-¡¼ »ó¼¼¼³¸í ¡½
-SNMP ¼­ºñ½º´Â ³×Æ®¿öÅ© °ü¸® ¹× ³×Æ®¿öÅ© ÀåÄ¡ÀÇ µ¿ÀÛÀ» °¨½Ã/ÅëÇÒÇÏ´Â SNMP ÇÁ·ÎÅäÄÝÀ» ±â¹ÝÀ¸·Î ÇÏ´Â ¼­ºñ½º·Î, SNMP Åë½Å ½Ã Á¢±Ù Çã¿ë ¿©ºÎ¸¦ °áÁ¤ÇÏ±â À§ÇÑ SNMP community stringÀÇ º¹Àâµµ°¡ ³·°Ô ¼³Á¤µÇ¾ú´ÂÁö Á¡°Ë
-
-¡¼ ÆÇ´Ü±âÁØ ¡½
-- ¾çÈ£ : SNMP Community String ÃÊ±â °ª(Public, Private)ÀÌ ¾Æ´Ï°í, ¾Æ·¡ÀÇ º¹Àâµµ¸¦ ¸¸Á· ÇÒ °æ¿ì
-- Ãë¾à : SNMP Community String ÃÊ±â °ª(Public, Private)ÀÌ°Å³ª, º¹Àâµµ¸¦ ¸¸Á·ÇÏÁö ¾ÊÀ» °æ¿ì
-
-¡Ø (º¹Àâµµ) ±âº»°ª(public, private) ¹Ì»ç¿ë, ¿µ¹®ÀÚ, ¼ýÀÚ°¡ Æ÷ÇÔ 10ÀÚ¸® ÀÌ»ó ¶Ç´Â ¿µ¹®ÀÚ, ¼ýÀÚ, Æ¯¼ö¹®ÀÚ Æ÷ÇÔ 8ÀÚ¸® ÀÌ»ó
-¡Ø SNMP v3ÀÇ °æ¿ì º°µµ ÀÎÁõ ±â´ÉÀ» »ç¿ëÇÏ°í, ÇØ´ç ºñ¹Ð¹øÈ£°¡ º¹Àâµµ¸¦ ¸¸Á·ÇÒ °æ¿ì "¾çÈ£"·Î ÆÇ´Ü
-
-¡¼ ÆÇ´Ü¹æ¹ý ¡½
-  1. SNMP ¼­ºñ½º ÀÇ SNMP Community String ÀÌ º¹Àâµµ¸¦ ¸¸Á·ÇÏ´ÂÁö È®ÀÎ
-      ¡Ø <registry_path> : HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\SNMP\Parameters\ValidCommunities
-      ¡Ø ·¹Áö½ºÆ®¸® °ªÀÌ Á¸ÀçÇÏÁö ¾ÊÀ¸¸é SNMP ¼­ºñ½º°¡ ½ÇÇà ÁßÀÌ¾îµµ ¼­ºñ½º¸¦ »ç¿ëÇÒ ¼ö ¾øÀ¸¹Ç·Î "¾çÈ£" ·Î ÆÇ´Ü
-
-  ¡á Windows 2008, 2008 R2, 2012, 2012 R2, 2016, 2019, 2022
-      ¨ç ½ÃÀÛ > ½ÇÇà > services.msc > ¼­ºñ½º > SNMP Service ¼Ó¼º > "º¸¾È" ÅÇ >
-      ¨è "¹Þ¾ÆµéÀÎ Ä¿¹Â´ÏÆ¼ ÀÌ¸§"  "> SNMP community string" Ç×¸ñ
-  ¶Ç´Â
-      cmd > reg query <registry_path>
-          <SNMP_community_string>     REG_DWORD    0x4
-  ¶Ç´Â
-      cmd > reg query <registry_path>
-          ERROR: The system was unable to find the specified registry key or value.
-
-  ¡Ø "ValidCommunities" Key ÀÇ Value °ª
-      - ÀÌ¸§ : 
-      - Á¾·ù : REG_DWORD
-      - µ¥ÀÌÅÍ : 1(¾øÀ½), 2(¾Ë¸²), 4(ÀÐ±â Àü¿ë) 8(ÀÐ±â, ¾²±â), 16(ÀÐ±â, ¸¸µé±â)
-
-¡¼ Á¶Ä¡¹æ¹ý ¡½
-  1. SNMP Community String À» º¹Àâµµ¸¦ ¸¸Á·ÇÏ´Â °ªÀ¸·Î ¼³Á¤
-      ¡Ø À¯ÃßÇÏ±â ½¬¿î ¹®ÀÚ¿­(±â°ü¸í, ±â±â¸í µî) »ç¿ë ÀÚÁ¦ ¡æ ÀÏºÎ ±â°ü¿¡¼­ ¼öÁ¤ ±Ç°íÇÏ´Â ±âÁØ
-      ¡Ø "¾²±â" ±ÇÇÑÀÌ ÇÊ¿äÇÑ °æ¿ì°¡ ¾Æ´Ï¸é "ÀÐ±â Àü¿ë" ±ÇÇÑ ºÎ¿©
-      ¡Ø NMS, ¸ð´ÏÅÍ¸µ µî ¸ñÀûÀ¸·Î SNMP ¼­ºñ½º¸¦ »ç¿ëÇÏ´Â °æ¿ì SNMP Manager ¿Í Agent ¿¡ °°Àº Community String ÀÌ ¼³Á¤µÇ¾î ÀÖ¾î¾ß ÀÎÁõ °¡´É
-  2. SNMP ¼­ºñ½º Àç½ÃÀÛ
+echo SRV-001 (Windows) SNMP Community ì„¤ì •ê°’ ë³´ì•ˆ ì ê²€
+echo ì ê²€ ë°°ê²½ ì„¤ëª…
+echo SNMP ì„œë¹„ìŠ¤ëŠ” ë„¤íŠ¸ì›Œí¬ ê´€ë¦¬ ë° ë„¤íŠ¸ì›Œí¬ ìœ„ì¹˜ì˜ ì •ë³´ë¥¼ ìˆ˜ì§‘/ê´€ë¦¬í•˜ëŠ” SNMP ê´€ë¦¬ ìŠ¤í…Œì´ì…˜ìœ¼ë¡œ, SNMP ë©”ì‹œì§€ ë° ìƒíƒœ ì •ë³´ë¥¼ êµí™˜í•˜ê¸° ìœ„í•œ SNMP community stringì˜ ë³´ì•ˆì„±ì„ ê°•í™”í•˜ê¸° ìœ„í•´ ì„¤ì •ê°’ì„ ì ê²€í•©ë‹ˆë‹¤.
+echo ì ê²€ ëŒ€ìƒ ë° ë°©ë²•
+echo - ì¡°ê±´ : SNMP Community String ê¸°ë³¸ ê°’(Public, Private)ì´ ì•„ë‹ˆë©°, ì•„ëž˜ì˜ ë³´ì•ˆì„±ì„ ë§Œì¡±í•˜ëŠ” ê²½ìš°
+echo - ê²°ê³¼ : SNMP Community String ê¸°ë³¸ ê°’(Public, Private)ì´ê±°ë‚˜, ë³´ì•ˆì„±ì„ ë§Œì¡±í•˜ì§€ ì•ŠëŠ” ê²½ìš°
+echo (ë³´ì•ˆì„±) ê¸°ë³¸ê°’(public, private) ì´ì™¸, ì•ŒíŒŒë²³, ìˆ«ìž, íŠ¹ìˆ˜ë¬¸ìžê°€ í˜¼í•©ëœ 10ìžë¦¬ ì´ìƒ í˜¹ì€ ì•ŒíŒŒë²³, ìˆ«ìžë§Œìœ¼ë¡œ êµ¬ì„±ëœ 8ìžë¦¬ ì´ìƒ
+echo SNMP v3ì˜ ê²½ìš° ë³´ì•ˆ ê°•í™”ë¥¼ ìœ„í•´ í•´ë‹¹ ì•”í˜¸í™”ë¥¼ ì‚¬ìš©í•˜ëŠ” ê²ƒì„ "ê¶Œìž¥"
+echo ì ê²€ ê²°ê³¼ í•´ì„
+echo 1. SNMP ì„œë¹„ìŠ¤ ë° SNMP Community Stringì˜ ë³´ì•ˆì„±ì„ ì ê²€í•˜ì‹œì˜¤.
+echo ëŒ€ì‘ Windows 2008, 2008 R2, 2012, 2012 R2, 2016, 2019, 2022
+echo ì ê²€ ë°©ì•ˆ ì œì‹œ
+echo 1. SNMP Community Stringì˜ ë³´ì•ˆì„±ì„ ê°•í™”í•˜ëŠ” ì„¤ì •ê°’ì„ ì ìš©í•˜ì‹­ì‹œì˜¤.
+echo 2. SNMP ì„œë¹„ìŠ¤ í™œì„±í™”
 echo -------------------------------------------end------------------------------------------
-
-echo --------------------------------------SRV-004 ºÒÇÊ¿äÇÑ SMTP ¼­ºñ½º ½ÇÇà ¿©ºÎ------------------------------------->> C:\Window_%COMPUTERNAME%_result\W-Window-%COMPUTERNAME%-rawdata.txt
+echo --------------------------------------SRV-004 ë³´ì•ˆ ê°•í™”ë¥¼ ìœ„í•œ SMTP ì„œë¹„ìŠ¤ ì ê²€ ------------------------------------->> C:\Window_%COMPUTERNAME%_result\W-Window-%COMPUTERNAME%-rawdata.txt
 sc query smtp >> C:\Window_%COMPUTERNAME%_result\W-Window-%COMPUTERNAME%-rawdata.txt
 echo ------------------------------------------------------------------------------->> C:\Window_%COMPUTERNAME%_result\W-Window-%COMPUTERNAME%-rawdata.txt
-s
