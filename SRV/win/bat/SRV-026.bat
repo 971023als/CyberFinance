5934169c984355e623eb0d4242e083f5451b7fb3
971023als <971023als@naver.com>
2024-02-12 15:47:00 +0900

    25 26

--- SRV/win/bat/SRV-026.bat
+++ SRV/win/bat/SRV-026.bat
@@ -1,20 +1,21 @@
-rem windows server script edit 2020
 @echo off
+chcp 65001 > nul
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 if '%errorlevel%' NEQ '0' (
-    echo °ü¸®ÀÚ ±ÇÇÑÀ» ¿äÃ»ÇÕ´Ï´Ù...
+    echo ê´€ë¦¬ìž ê¶Œí•œì´ í•„ìš”í•©ë‹ˆë‹¤...
     goto UACPrompt
 ) else ( goto gotAdmin )
+
 :UACPrompt
     echo Set UAC = CreateObject^("Shell.Application"^) > "%getadmin.vbs"
     set params = %*:"=""
-    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "getadmin.vbs"
-    "getadmin.vbs"
-	del "getadmin.vbs"
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%getadmin.vbs"
+    "%getadmin.vbs"
+	del "%getadmin.vbs"
     exit /B
 
 :gotAdmin
-chcp 437
+chcp 65001
 color 02
 setlocal enabledelayedexpansion
 echo ------------------------------------------Setting---------------------------------------
@@ -54,46 +55,8 @@ for /F "tokens=5 delims=*" %%a in ('type C:\Window_%COMPUTERNAME%_raw\line.txt')
 type C:\WINDOWS\system32\inetsrv\MetaBase.xml >> C:\Window_%COMPUTERNAME%_raw\iis_setting.txt
 echo ------------------------------------------end-------------------------------------------
 echo ------------------------------------------SRV-001------------------------------------------
-SRV-001 (Windows) SNMP Community ½ºÆ®¸µ ¼³Á¤ ¹ÌÈí
-
-¡¼ »ó¼¼¼³¸í ¡½
-SNMP ¼­ºñ½º´Â ³×Æ®¿öÅ© °ü¸® ¹× ³×Æ®¿öÅ© ÀåÄ¡ÀÇ µ¿ÀÛÀ» °¨½Ã/ÅëÇÒÇÏ´Â SNMP ÇÁ·ÎÅäÄÝÀ» ±â¹ÝÀ¸·Î ÇÏ´Â ¼­ºñ½º·Î, SNMP Åë½Å ½Ã Á¢±Ù Çã¿ë ¿©ºÎ¸¦ °áÁ¤ÇÏ±â À§ÇÑ SNMP community stringÀÇ º¹Àâµµ°¡ ³·°Ô ¼³Á¤µÇ¾ú´ÂÁö Á¡°Ë
-
-¡¼ ÆÇ´Ü±âÁØ ¡½
-- ¾çÈ£ : SNMP Community String ÃÊ±â °ª(Public, Private)ÀÌ ¾Æ´Ï°í, ¾Æ·¡ÀÇ º¹Àâµµ¸¦ ¸¸Á· ÇÒ °æ¿ì
-- Ãë¾à : SNMP Community String ÃÊ±â °ª(Public, Private)ÀÌ°Å³ª, º¹Àâµµ¸¦ ¸¸Á·ÇÏÁö ¾ÊÀ» °æ¿ì
-
-¡Ø (º¹Àâµµ) ±âº»°ª(public, private) ¹Ì»ç¿ë, ¿µ¹®ÀÚ, ¼ýÀÚ°¡ Æ÷ÇÔ 10ÀÚ¸® ÀÌ»ó ¶Ç´Â ¿µ¹®ÀÚ, ¼ýÀÚ, Æ¯¼ö¹®ÀÚ Æ÷ÇÔ 8ÀÚ¸® ÀÌ»ó
-¡Ø SNMP v3ÀÇ °æ¿ì º°µµ ÀÎÁõ ±â´ÉÀ» »ç¿ëÇÏ°í, ÇØ´ç ºñ¹Ð¹øÈ£°¡ º¹Àâµµ¸¦ ¸¸Á·ÇÒ °æ¿ì "¾çÈ£"·Î ÆÇ´Ü
-
-¡¼ ÆÇ´Ü¹æ¹ý ¡½
-  1. SNMP ¼­ºñ½º ÀÇ SNMP Community String ÀÌ º¹Àâµµ¸¦ ¸¸Á·ÇÏ´ÂÁö È®ÀÎ
-      ¡Ø <registry_path> : HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\SNMP\Parameters\ValidCommunities
-      ¡Ø ·¹Áö½ºÆ®¸® °ªÀÌ Á¸ÀçÇÏÁö ¾ÊÀ¸¸é SNMP ¼­ºñ½º°¡ ½ÇÇà ÁßÀÌ¾îµµ ¼­ºñ½º¸¦ »ç¿ëÇÒ ¼ö ¾øÀ¸¹Ç·Î "¾çÈ£" ·Î ÆÇ´Ü
-
-  ¡á Windows 2008, 2008 R2, 2012, 2012 R2, 2016, 2019, 2022
-      ¨ç ½ÃÀÛ > ½ÇÇà > services.msc > ¼­ºñ½º > SNMP Service ¼Ó¼º > "º¸¾È" ÅÇ >
-      ¨è "¹Þ¾ÆµéÀÎ Ä¿¹Â´ÏÆ¼ ÀÌ¸§"  "> SNMP community string" Ç×¸ñ
-  ¶Ç´Â
-      cmd > reg query <registry_path>
-          <SNMP_community_string>     REG_DWORD    0x4
-  ¶Ç´Â
-      cmd > reg query <registry_path>
-          ERROR: The system was unable to find the specified registry key or value.
-
-  ¡Ø "ValidCommunities" Key ÀÇ Value °ª
-      - ÀÌ¸§ : 
-      - Á¾·ù : REG_DWORD
-      - µ¥ÀÌÅÍ : 1(¾øÀ½), 2(¾Ë¸²), 4(ÀÐ±â Àü¿ë) 8(ÀÐ±â, ¾²±â), 16(ÀÐ±â, ¸¸µé±â)
-
-¡¼ Á¶Ä¡¹æ¹ý ¡½
-  1. SNMP Community String À» º¹Àâµµ¸¦ ¸¸Á·ÇÏ´Â °ªÀ¸·Î ¼³Á¤
-      ¡Ø À¯ÃßÇÏ±â ½¬¿î ¹®ÀÚ¿­(±â°ü¸í, ±â±â¸í µî) »ç¿ë ÀÚÁ¦ ¡æ ÀÏºÎ ±â°ü¿¡¼­ ¼öÁ¤ ±Ç°íÇÏ´Â ±âÁØ
-      ¡Ø "¾²±â" ±ÇÇÑÀÌ ÇÊ¿äÇÑ °æ¿ì°¡ ¾Æ´Ï¸é "ÀÐ±â Àü¿ë" ±ÇÇÑ ºÎ¿©
-      ¡Ø NMS, ¸ð´ÏÅÍ¸µ µî ¸ñÀûÀ¸·Î SNMP ¼­ºñ½º¸¦ »ç¿ëÇÏ´Â °æ¿ì SNMP Manager ¿Í Agent ¿¡ °°Àº Community String ÀÌ ¼³Á¤µÇ¾î ÀÖ¾î¾ß ÀÎÁõ °¡´É
-  2. SNMP ¼­ºñ½º Àç½ÃÀÛ
+echo SRV-001 (Windows) SNMP Community ì„¤ì • ê²€ì‚¬
 echo -------------------------------------------end------------------------------------------
-
-echo --------------------------------------SRV-004 ºÒÇÊ¿äÇÑ SMTP ¼­ºñ½º ½ÇÇà ¿©ºÎ------------------------------------->> C:\Window_%COMPUTERNAME%_result\W-Window-%COMPUTERNAME%-rawdata.txt
+echo --------------------------------------SRV-004 í•„ìˆ˜ì ì¸ SMTP ì„œë¹„ìŠ¤ ìƒíƒœ í™•ì¸------------------------------------->> C:\Window_%COMPUTERNAME%_result\W-Window-%COMPUTERNAME%-rawdata.txt
 sc query smtp >> C:\Window_%COMPUTERNAME%_result\W-Window-%COMPUTERNAME%-rawdata.txt
 echo ------------------------------------------------------------------------------->> C:\Window_%COMPUTERNAME%_result\W-Window-%COMPUTERNAME%-rawdata.txt
