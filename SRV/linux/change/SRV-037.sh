47e9ba2f16b77d9db0fe39c4433fe5399f414a5f
971023als <971023als@naver.com>
2024-02-18 20:43:34 +0900

    Update SRV-037.sh

--- SRV/linux/change/SRV-037.sh
+++ SRV/linux/change/SRV-037.sh
@@ -7,80 +7,37 @@ TMP1=$(SCRIPTNAME).log
 
 BAR
 
-CODE [SRV-037] 취약한 FTP 서비스 실행
+echo "[SRV-037] 취약한 FTP 서비스 실행 조치" >> $TMP1
 
-cat << EOF >> $TMP1
-[양호]: FTP 서비스가 비활성화 되어 있는 경우
-[취약]: FTP 서비스가 활성화 되어 있는 경우
-EOF
+# FTP 서비스 비활성화
+# 시스템에서 사용 중인 FTP 데몬을 확인하고 비활성화합니다.
+# vsftpd와 proftpd 예시로 비활성화
+FTP_SERVICES=("vsftpd" "proftpd")
+
+for service in "${FTP_SERVICES[@]}"; do
+    if systemctl is-enabled --quiet $service; then
+        systemctl stop $service
+        systemctl disable $service
+        echo "조치: $service 서비스가 비활성화되었습니다." >> $TMP1
+    else
+        echo "조치: $service 서비스가 이미 비활성화되어 있습니다." >> $TMP1
+    fi
+done
+
+# /etc/xinetd.d 내 FTP 관련 서비스 파일 비활성화
+if [ -d /etc/xinetd.d ]; then
+    for service_file in /etc/xinetd.d/*ftp*; do
+        if [ -f "$service_file" ]; then
+            sed -i 's/disable[ \t]*=[ \t]*no/disable = yes/' "$service_file"
+            echo "조치: $service_file 내 서비스가 비활성화되었습니다." >> $TMP1
+        fi
+    done
+fi
+
+echo "모든 취약한 FTP 서비스에 대한 비활성화 조치가 완료되었습니다." >> $TMP1
 
 BAR
 
-if [ -f /etc/services ]; then
-		ftp_port_count=`grep -vE '^#|^\s#' /etc/services | awk 'tolower($1)=="ftp" {print $2}' | awk -F / 'tolower($2)=="tcp" {print $1}' | wc -l`
-		if [ $ftp_port_count -gt 0 ]; then
-			ftp_port=(`grep -vE '^#|^\s#' /etc/services | awk 'tolower($1)=="ftp" {print $2}' | awk -F / 'tolower($2)=="tcp" {print $1}'`)
-			for ((i=0; i<${#ftp_port[@]}; i++))
-			do
-				netstat_ftp_count=`netstat -nat 2>/dev/null | grep -w 'tcp' | grep -Ei 'listen|established|syn_sent|syn_received' | grep ":${ftp_port[$i]} " | wc -l`
-				if [ $netstat_ftp_count -gt 0 ]; then
-					WARN " ftp 서비스가 실행 중입니다." >> $TMP1
-					return 0
-				fi
-			done
-		fi
-	fi
-	find_vsftpdconf_count=`find / -name 'vsftpd.conf' -type f 2>/dev/null | wc -l`
-	if [ $find_vsftpdconf_count -gt 0 ]; then
-		vsftpdconf_files=(`find / -name 'vsftpd.conf' -type f 2>/dev/null`)
-		for ((i=0; i<${#vsftpdconf_files[@]}; i++))
-		do
-			if [ -f ${vsftpdconf_files[$i]} ]; then
-				vsftpdconf_file_port_count=`grep -vE '^#|^\s#' ${vsftpdconf_files[$i]} | grep 'listen_port' | awk -F = '{gsub(" ", "", $0); print $2}' | wc -l`
-				if [ $vsftpdconf_file_port_count -gt 0 ]; then
-					ftp_port=(`grep -vE '^#|^\s#' ${vsftpdconf_files[$i]} | grep 'listen_port' | awk -F = '{gsub(" ", "", $0); print $2}'`)
-					for ((j=0; j<${#ftp_port[@]}; j++))
-					do
-						netstat_ftp_count=`netstat -nat 2>/dev/null | grep -w 'tcp' | grep -Ei 'listen|established|syn_sent|syn_received' | grep ":${ftp_port[$j]} " | wc -l`
-						if [ $netstat_ftp_count -gt 0 ]; then
-							WARN " ftp 서비스가 실행 중입니다." >> $TMP1
-							return 0
-						fi
-					done
-				fi
-			fi
-		done
-	fi
-	find_proftpdconf_count=`find / -name 'proftpd.conf' -type f 2>/dev/null | wc -l`
-	if [ $find_proftpdconf_count -gt 0 ]; then
-		proftpdconf_files=(`find / -name 'proftpd.conf' -type f 2>/dev/null`)
-		for ((i=0; i<${#proftpdconf_files[@]}; i++))
-		do
-			if [ -f ${proftpdconf_files[$i]} ]; then
-				proftpdconf_file_port_count=`grep -vE '^#|^\s#' ${proftpdconf_files[$i]} | grep 'Port' | awk '{print $2}' | wc -l`
-				if [ $proftpdconf_file_port_count -gt 0 ]; then
-					ftp_port=(`grep -vE '^#|^\s#' ${proftpdconf_files[$i]} | grep 'Port' | awk '{print $2}'`)
-					for ((j=0; j<${#ftp_port[@]}; j++))
-					do
-						netstat_ftp_count=`netstat -nat 2>/dev/null | grep -w 'tcp' | grep -Ei 'listen|established|syn_sent|syn_received' | grep ":${ftp_port[$j]} " | wc -l`
-						if [ $netstat_ftp_count -gt 0 ]; then
-							WARN " ftp 서비스가 실행 중입니다." >> $TMP1
-							return 0
-						fi
-					done
-				fi
-			fi
-		done
-	fi
-	ps_ftp_count=`ps -ef | grep -iE 'ftp|vsftpd|proftp' | grep -v 'grep' | wc -l`
-	if [ $ps_ftp_count -gt 0 ]; then
-		WARN " ftp 서비스가 실행 중입니다." >> $TMP1
-		return 0
-	else
-		OK "※ U-61 결과 : 양호(Good)" >> $TMP1
-		return 0
-	fi
-
-cat $TMP1
+cat "$TMP1"
 
 echo ; echo
