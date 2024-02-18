07929f9d11304fc6e0fe62eaf9db7961dcf97b21
971023als <971023als@naver.com>
2024-02-19 00:03:06 +0900

    122 123

--- SRV/linux/change/SRV-123.sh
+++ SRV/linux/change/SRV-123.sh
@@ -1,36 +1,28 @@
 #!/bin/bash
 
-. function.sh
-
-TMP1=$(SCRIPTNAME).log
+# 결과 파일 초기화
+TMP1="$(SCRIPTNAME).log"
 > $TMP1
 
-BAR
-
-CODE [SRV-123] 최종 로그인 사용자 계정 노출
-
-cat << EOF >> $result
-[양호]: 최종 로그인 사용자 정보가 노출되지 않는 경우
-[취약]: 최종 로그인 사용자 정보가 노출되는 경우
-EOF
-
-BAR
+echo "최종 로그인 사용자 계정 노출 점검" >> $TMP1
+echo "=================================" >> $TMP1
 
 # 로그인 메시지 파일 확인
 files=("/etc/motd" "/etc/issue" "/etc/issue.net")
 
 for file in "${files[@]}"; do
   if [ -f "$file" ]; then
+    # 'Last login' 문자열 검사
     if grep -q 'Last login' "$file"; then
-      WARN "파일 $file 에 최종 로그인 사용자 정보가 포함되어 있습니다."
+      echo "WARN: 파일 $file 에 최종 로그인 사용자 정보가 포함되어 있습니다." >> $TMP1
     else
-      OK "파일 $file 에 최종 로그인 사용자 정보가 포함되지 않았습니다."
+      echo "OK: 파일 $file 에 최종 로그인 사용자 정보가 포함되지 않았습니다." >> $TMP1
     fi
   else
-    INFO "파일 $file 이(가) 존재하지 않습니다."
+    echo "INFO: 파일 $file 이(가) 존재하지 않습니다." >> $TMP1
   fi
 done
 
-cat $result
-
-echo ; echo
+# 결과 파일 출력
+cat $TMP1
+echo
