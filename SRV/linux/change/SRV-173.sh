8a6675b7204573e7062149e40f587b972e8e2951
971023als <971023als@naver.com>
2024-02-19 02:37:56 +0900

    Update SRV-173.sh

--- SRV/linux/change/SRV-173.sh
+++ SRV/linux/change/SRV-173.sh
@@ -9,12 +9,7 @@ BAR
 
 CODE [SRV-173] DNS 서비스의 취약한 동적 업데이트 설정
 
-cat << EOF >> $result
-[양호]: DNS 동적 업데이트가 안전하게 구성된 경우
-[취약]: DNS 동적 업데이트가 취약하게 구성된 경우
-EOF
-
-BAR
+echo "DNS 서비스의 동적 업데이트 설정을 확인합니다..."
 
 # DNS 설정 파일 경로
 dns_config="/etc/bind/named.conf"
@@ -23,14 +18,27 @@ dns_config="/etc/bind/named.conf"
 if [ -f "$dns_config" ]; then
     dynamic_updates=$(grep "allow-update" "$dns_config")
     if [ -z "$dynamic_updates" ]; then
-        OK "DNS 동적 업데이트가 안전하게 구성되어 있습니다."
+        echo "DNS 동적 업데이트가 안전하게 구성되어 있습니다."
+        OK "DNS 동적 업데이트가 안전하게 구성되어 있습니다." >> $TMP1
     else
-        WARN "DNS 동적 업데이트 설정이 취약합니다: $dynamic_updates"
+        echo "DNS 동적 업데이트 설정이 취약합니다: $dynamic_updates"
+        read -p "동적 업데이트 설정을 제거(안전하게 수정)하시겠습니까? (y/n): " user_decision
+        
+        if [ "$user_decision" == "y" ]; then
+            # 동적 업데이트 설정 제거
+            sed -i '/allow-update/d' "$dns_config"
+            echo "DNS 동적 업데이트 설정을 안전하게 수정했습니다."
+            OK "DNS 동적 업데이트가 안전하게 수정되었습니다." >> $TMP1
+        else
+            echo "동적 업데이트 설정 수정 작업이 취소되었습니다."
+            WARN "DNS 동적 업데이트 설정이 취약합니다." >> $TMP1
+        fi
     fi
 else
-    INFO "DNS 설정 파일이 존재하지 않습니다."
+    echo "DNS 설정 파일이 존재하지 않습니다."
+    INFO "DNS 설정 파일이 존재하지 않습니다." >> $TMP1
 fi
 
-cat $result
+cat $TMP1
 
 echo ; echo
