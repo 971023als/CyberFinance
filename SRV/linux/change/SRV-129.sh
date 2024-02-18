c5a632db3b5d758a4cf93731ec840369c779ca8c
971023als <971023als@naver.com>
2024-02-19 01:52:19 +0900

    Update SRV-129.sh

--- SRV/linux/change/SRV-129.sh
+++ SRV/linux/change/SRV-129.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
 
-. function.sh
-
-TMP1=$(SCRIPTNAME).log
+# 결과 파일 초기화
+TMP1="$(SCRIPTNAME).log"
 > $TMP1
 
-BAR
-
-CODE [SRV-129] 백신 프로그램 미설치
-
-cat << EOF >> $result
-[양호]: 백신 프로그램이 설치되어 있는 경우
-[취약]: 백신 프로그램이 설치되어 있지 않은 경우
-EOF
-
-BAR
+echo "백신 프로그램 설치 여부 점검" >> $TMP1
+echo "=========================" >> $TMP1
 
 # 일반적으로 사용되는 백신 프로그램의 설치 여부를 확인합니다
 antivirus_programs=("clamav" "avast" "avg" "avira" "eset")
@@ -29,11 +20,11 @@ done
 
 # 설치된 백신 프로그램이 있는지 확인합니다
 if [ ${#installed_antivirus[@]} -eq 0 ]; then
-  WARN "설치된 백신 프로그램이 없습니다."
+  echo "WARN: 설치된 백신 프로그램이 없습니다." >> $TMP1
 else
-  OK "설치된 백신 프로그램: ${installed_antivirus[*]}"
+  echo "OK: 설치된 백신 프로그램: ${installed_antivirus[*]}" >> $TMP1
 fi
 
-cat $result
-
-echo ; echo
+# 결과 출력
+cat $TMP1
+echo
