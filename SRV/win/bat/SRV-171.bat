f1e674b8b2c87fb8fcbf244ba56749118a3f046f
971023als <971023als@naver.com>
2024-02-20 14:52:01 +0900

    171 172

--- SRV/win/bat/SRV-171.bat
+++ SRV/win/bat/SRV-171.bat
@@ -1,46 +1,19 @@
-#!/bin/bash
+@echo off
+SetLocal EnableDelayedExpansion
 
-. function.sh
+set "SCRIPTNAME=%~n0"
+set "TMP1=%SCRIPTNAME%.log"
 
-TMP1=$(SCRIPTNAME).log
-> $TMP1
+:: 로그 파일 초기화
+type NUL > "%TMP1%"
 
-BAR
+echo BAR >> "%TMP1%"
+echo CODE [SRV-171] FTP 서비스 정보 노출 >> "%TMP1%"
+echo [양호]: FTP 서버에서 버전 정보 및 기타 세부 정보가 노출되지 않는 경우 >> "%TMP1%"
+echo [취약]: FTP 서버에서 버전 정보 및 기타 세부 정보가 노출되는 경우 >> "%TMP1%"
+echo BAR >> "%TMP1%"
 
-CODE [SRV-171] FTP 서비스 정보 노출
-
-cat << EOF >> $result
-[양호]: FTP 서버에서 버전 정보 및 기타 세부 정보가 노출되지 않는 경우
-[취약]: FTP 서버에서 버전 정보 및 기타 세부 정보가 노출되는 경우
-EOF
-
-BAR
-
-# FTP 서버 설정 확인 (예: vsftpd, proftpd 등)
-# vsftpd 예시
-vsftpd_config="/etc/vsftpd.conf"
-if [ -f "$vsftpd_config" ]; then
-    if grep -q 'ftpd_banner=' "$vsftpd_config"; then
-        OK "vsftpd에서 버전 정보 노출이 제한됩니다."
-    else
-        WARN "vsftpd에서 버전 정보가 노출됩니다."
-    fi
-else
-    INFO "vsftpd 설정 파일이 존재하지 않습니다."
-fi
-
-# ProFTPD 예시
-proftpd_config="/etc/proftpd/proftpd.conf"
-if [ -f "$proftpd_config" ]; then
-    if grep -q 'ServerIdent on "FTP Server ready."' "$proftpd_config"; then
-        OK "ProFTPD에서 버전 정보 노출이 제한됩니다."
-    else
-        WARN "ProFTPD에서 버전 정보가 노출됩니다."
-    fi
-else
-    INFO "ProFTPD 설정 파일이 존재하지 않습니다."
-fi
-
-cat $result
-
-echo ; echo
+:: FTP 서비스 실행 상태 확인
+for /f "tokens=*" %%i in ('sc query ftpsvc ^| findstr /C:"RUNNING"') do (
+    set "serviceStatus=%%i"
+    if not "!serviceStatus!"==
--- SRV/win/bat/SRV-172.bat
+++ SRV/win/bat/SRV-172.bat
@@ -1,37 +1,31 @@
-#!/bin/bash
+@echo off
+SetLocal EnableDelayedExpansion
 
-. function.sh
+set "SCRIPTNAME=%~n0"
+set "TMP1=%SCRIPTNAME%.log"
 
-TMP1=$(SCRIPTNAME).log
-> $TMP1
+:: 로그 파일 초기화
+type NUL > "%TMP1%"
 
-BAR
+echo BAR >> "%TMP1%"
+echo CODE [SRV-172] 불필요한 시스템 자원 공유 존재 >> "%TMP1%"
+echo [양호]: 불필요한 시스템 자원이 공유되지 않는 경우 >> "%TMP1%"
+echo [취약]: 불필요한 시스템 자원이 공유되는 경우 >> "%TMP1%"
+echo BAR >> "%TMP1%"
 
-CODE [SRV-172] 불필요한 시스템 자원 공유 존재
+:: Windows 공유 상태 확인
+for /f "tokens=1" %%i in ('net share ^| findstr /R /C:"^C " /C:"^D " /C:"^E "') do (
+    set "share=%%i"
+    if not "!share!"=="" (
+        echo WARN 불필요한 시스템 자원이 공유됩니다: !share! >> "%TMP1%"
+        set "sharesFound=true"
+    )
+)
 
-cat << EOF >> $result
-[양호]: 불필요한 시스템 자원이 공유되지 않는 경우
-[취약]: 불필요한 시스템 자원이 공유되는 경우
-EOF
+if not defined sharesFound (
+    echo OK 불필요한 시스템 자원이 공유되지 않습니다. >> "%TMP1%"
+)
 
-BAR
+type "%TMP1%"
 
-# NFS 공유 확인
-nfs_shares=$(showmount -e localhost)
-if [ -z "$nfs_shares" ]; then
-    OK "NFS에서 불필요한 공유가 존재하지 않습니다."
-else
-    WARN "NFS에서 다음 공유가 발견되었습니다: $nfs_shares"
-fi
-
-# Samba 공유 확인
-samba_shares=$(smbstatus -S)
-if [ -z "$samba_shares" ]; then
-    OK "Samba에서 불필요한 공유가 존재하지 않습니다."
-else
-    WARN "Samba에서 다음 공유가 발견되었습니다: $samba_shares"
-fi
-
-cat $result
-
-echo ; echo
+EndLocal
