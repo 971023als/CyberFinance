d02d23416cf6fb59a214b5dc740e01b910ccf123
971023als <971023als@naver.com>
2024-02-22 00:57:44 +0900

    43 44

--- SRV/win/bat/SRV-043.bat
+++ SRV/win/bat/SRV-043.bat
@@ -4,25 +4,25 @@ setlocal
 set "TMP1=%~n0.log"
 > "%TMP1%"
 
-echo CODE [SRV-043] Unnecessary Files in Web Service Path >> "%TMP1%"
-echo [Good]: No unnecessary files exist within the web service path >> "%TMP1%"
-echo [Vulnerable]: Unnecessary files exist within the web service path >> "%TMP1%"
+echo 코드 [SRV-043] 웹 서비스 경로 내 불필요한 파일 존재 >> "%TMP1%"
+echo [양호]: 웹 서비스 경로 내 불필요한 파일이 존재하지 않음 >> "%TMP1%"
+echo [취약]: 웹 서비스 경로 내 불필요한 파일이 존재함 >> "%TMP1%"
 
-:: Set the default IIS web service path
+:: 기본 IIS 웹 서비스 경로 설정
 set "WEB_SERVICE_PATH=C:\inetpub\wwwroot"
 
-:: List common unnecessary files within the web service path
-echo Checking for common unnecessary files within %WEB_SERVICE_PATH%: >> "%TMP1%"
+:: 웹 서비스 경로 내 흔히 발견되는 불필요한 파일 목록 작성
+echo %WEB_SERVICE_PATH% 내 흔히 발견되는 불필요한 파일 검사 중: >> "%TMP1%"
 dir "%WEB_SERVICE_PATH%\*.bak" /s /b >> "%TMP1%"
 dir "%WEB_SERVICE_PATH%\*.tmp" /s /b >> "%TMP1%"
 dir "%WEB_SERVICE_PATH%\*test*" /s /b >> "%TMP1%"
 dir "%WEB_SERVICE_PATH%\*example*" /s /b >> "%TMP1%"
 
-:: Note: This script lists files based on common patterns. Please review them manually.
-echo Note: Please manually review the listed files to ensure they are necessary. >> "%TMP1%"
+:: 주의: 이 스크립트는 흔한 패턴을 기반으로 파일을 나열합니다. 수동으로 검토해 주세요.
+echo 주의: 나열된 파일을 수동으로 검토하여 필요한 파일인지 확인해 주세요. >> "%TMP1%"
 
-:: Display the results
+:: 결과 표시
 type "%TMP1%"
 
 echo.
-echo Script complete.
+echo 스크립트 완료.
