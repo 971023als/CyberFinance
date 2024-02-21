81ecd8d92e9103d16be7e7029d465d7898539b2f
971023als <971023als@naver.com>
2024-02-22 01:49:35 +0900

    Update SRV-059.bat

--- SRV/win/bat/SRV-059.bat
+++ SRV/win/bat/SRV-059.bat
@@ -1,35 +1,35 @@
 @echo off
-setlocal
+setlocal enabledelayedexpansion
 
-set "TMP1=%SCRIPTNAME%.log"
-> "%TMP1%"
+set "TMP1=%~n0.log"
+type nul > "!TMP1!"
 
-echo 코드 [SRV-059] 웹 서비스 서버 명령 실행 기능 제한 미흡 >> "%TMP1%"
-echo [양호]: 웹 서비스에서 서버 명령 실행 기능이 적절하게 제한됨 >> "%TMP1%"
-echo [취약]: 웹 서비스에서 서버 명령 실행 기능이 불충분하게 제한됨 >> "%TMP1%"
+echo ------------------------------------------------ >> "!TMP1!"
+echo 코드 [SRV-059] 웹 서비스 파일 업로드 및 다운로드 크기 제한 미설정 >> "!TMP1!"
+echo [양호]: 웹 서비스에서 파일 업로드 및 다운로드 크기가 적절하게 제한됨 >> "!TMP1!"
+echo [취약]: 웹 서비스에서 파일 업로드 및 다운로드 크기가 제한되지 않음 >> "!TMP1!"
+echo ------------------------------------------------ >> "!TMP1!"
 
-:: Windows에서 Apache와 Nginx 설정 파일 경로 설정
-set "APACHE_CONFIG_FILE=C:\path\to\apache\conf\httpd.conf"
-set "NGINX_CONFIG_FILE=C:\path\to\nginx\conf\nginx.conf"
+:: IIS 웹 서비스의 각 사이트에 대한 파일 업로드 크기 제한을 PowerShell을 사용하여 확인
+powershell -Command "& {
+    Import-Module WebAdministration;
+    $sites = Get-Website;
+    foreach ($site in $sites) {
+        $siteName = $site.Name;
+        $configPath = 'IIS:\Sites\' + $siteName;
+        $requestFiltering = Get-WebConfigurationProperty -pspath $configPath -filter 'system.webServer/security/requestFiltering/requestLimits' -name 'maxAllowedContentLength';
+        
+        $maxSize = $requestFiltering.Value / 1024 / 1024; # 바이트에서 MB로 변환
+        if ($maxSize -le 100) { # 예시 임계값 100MB
+            echo OK: "!siteName!" 사이트는 파일 업로드를 "!maxSize! MB"로 제한합니다. >> "!TMP1!"
+        } else {
+            echo WARN: "!siteName!" 사이트는 파일 업로드 제한이 높음 ("!maxSize! MB") 또는 설정되지 않음. >> "!TMP1!"
+        }
+    }
+}"
 
-:: Apache에서 서버 명령 실행 제한 확인
-findstr /R "^[ \t]*ScriptAlias" "%APACHE_CONFIG_FILE%" >nul
-if not errorlevel 1 (
-    echo 경고: Apache에서 서버 명령 실행이 허용될 수 있음: %APACHE_CONFIG_FILE% >> "%TMP1%"
-) else (
-    echo OK: Apache에서 서버 명령 실행 기능이 적절하게 제한됨: %APACHE_CONFIG_FILE% >> "%TMP1%"
-)
-
-:: Nginx에서 FastCGI 스크립트 실행 제한 확인
-findstr /R "fastcgi_pass" "%NGINX_CONFIG_FILE%" >nul
-if not errorlevel 1 (
-    echo 경고: Nginx에서 FastCGI를 통한 서버 명령 실행이 허용될 수 있음: %NGINX_CONFIG_FILE% >> "%TMP1%"
-) else (
-    echo OK: Nginx에서 서버 명령 실행 기능이 적절하게 제한됨: %NGINX_CONFIG_FILE% >> "%TMP1%"
-)
-
-:: 결과 표시
-type "%TMP1%"
+echo ------------------------------------------------ >> "!TMP1!"
+type "!TMP1!"
 
 echo.
 echo 스크립트 완료.
