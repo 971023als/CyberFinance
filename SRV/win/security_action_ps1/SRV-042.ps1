d40417b3f2ad9ca6c4e602b66c3a1bbe2810c73a
971023als <971023als@naver.com>
2024-02-24 01:58:27 +0900

    25

--- SRV/win/security_action_ps1/SRV-042.ps1
+++ SRV/win/security_action_ps1/SRV-042.ps1
@@ -1,53 +1,33 @@
-# 결과 파일 초기화
-$TMP1 = "$(Get-Location)\$(($MyInvocation.MyCommand.Name).Replace('.ps1', '.log'))"
-"" | Set-Content $TMP1
+@echo off
+setlocal enabledelayedexpansion
 
-Function BAR {
-    "-------------------------------------------------" | Out-File -FilePath $TMP1 -Append
-}
+set "TMP1=%~n0.log"
+type nul > !TMP1!
 
-Function CODE {
-    Param ([string]$message)
-    $message | Out-File -FilePath $TMP1 -Append
-}
+echo ------------------------------------------------ >> !TMP1!
+echo CODE [SRV-042] 웹 서비스 상위 디렉터리 접근 제한 설정 미흡 >> !TMP1!
+echo ------------------------------------------------ >> !TMP1!
 
-Function OK {
-    Param ([string]$message)
-    "OK: $message" | Out-File -FilePath $TMP1 -Append
-}
+echo [양호]: DocumentRoot가 별도의 보안 디렉터리로 지정된 경우 >> !TMP1!
+echo [취약]: DocumentRoot가 기본 디렉터리 또는 민감한 디렉터리로 지정된 경우 >> !TMP1!
+echo ------------------------------------------------ >> !TMP1!
 
-Function WARN {
-    Param ([string]$message)
-    "WARN: $message" | Out-File -FilePath $TMP1 -Append
-}
-
-BAR
-
-CODE "[SRV-040] 웹 서비스 디렉터리 리스팅 방지"
-
-BAR
-
-@"
-[양호]: 웹 서비스 디렉터리 리스팅이 적절하게 방지된 경우
-[취약]: 웹 서비스 디렉터리 리스팅 방지 설정이 미흡한 경우
-"@ | Out-File -FilePath $TMP1 -Append
-
-BAR
-
-# IIS 웹 사이트의 디렉터리 브라우징 설정을 점검합니다.
-Import-Module WebAdministration
-
-$websites = Get-Website
-foreach ($website in $websites) {
-    $directoryBrowsing = Get-WebConfigurationProperty -pspath "IIS:\Sites\$($website.name)" -filter "system.webServer/directoryBrowse" -name "enabled"
-    if ($directoryBrowsing.Value -eq $true) {
-        WARN "웹 사이트 '$($website.name)'에서 디렉터리 리스팅이 활성화되어 있습니다."
-    } else {
-        OK "웹 사이트 '$($website.name)'에서 디렉터리 리스팅이 비활성화되어 있습니다."
+:: 디렉터리 접근 권한 설정 확인 (PowerShell 사용)
+powershell -Command "& {
+    Import-Module WebAdministration;
+    $sites = Get-Website | foreach { $_.Name };
+    foreach ($site in $sites) {
+        $rootPath = Get-WebApplication -Site $site | Select -ExpandProperty PhysicalPath;
+        $accessPolicy = Get-WebConfigurationProperty -pspath 'IIS:\Sites\'$site -filter 'system.webServer/security/authorization' -name '.Collection';
+        if ($accessPolicy.Count -eq 0) {
+            Add-Content !TMP1! ('WARN: 웹 사이트 '''+$site+'''에서 상위 디렉터리 접근 제한 설정이 미흡합니다. 루트 경로: '+$rootPath);
+        } else {
+            Add-Content !TMP1! ('OK: 웹 사이트 '''+$site+'''에서 상위 디렉터리 접근 제한 설정이 적절합니다. 루트 경로: '+$rootPath);
+        }
     }
-}
+}"
 
-BAR
+echo ------------------------------------------------ >> !TMP1!
+type !TMP1!
 
-# 최종 결과를 출력합니다.
-Get-Content $TMP1 | Write-Output
+echo.
