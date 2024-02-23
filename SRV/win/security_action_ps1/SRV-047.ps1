1f5215d15573a627e759b3976f16a567f849f1ee
971023als <971023als@naver.com>
2024-02-24 02:04:31 +0900

    Update SRV-047.ps1

--- SRV/win/security_action_ps1/SRV-047.ps1
+++ SRV/win/security_action_ps1/SRV-047.ps1
@@ -1,42 +1,26 @@
-# 결과 파일 초기화
-$TMP1 = "$(Get-Location)\SRV-048_log.txt"
-"" | Set-Content $TMP1
+@echo off
+setlocal
 
-Function Write-BAR {
-    "-------------------------------------------------" | Out-File -FilePath $TMP1 -Append
-}
+set "TMP1=%~n0.log"
+> "%TMP1%"
 
-Function Write-Result {
-    Param ([string]$message)
-    $message | Out-File -FilePath $TMP1 -Append
-}
+echo 코드 [SRV-047] 웹 서비스 경로 내 불필요한 링크 파일 존재 >> "%TMP1%"
+echo [양호]: 웹 서비스 경로 내 불필요한 심볼릭 링크 파일이 존재하지 않음 >> "%TMP1%"
+echo [취약]: 웹 서비스 경로 내 불필요한 심볼릭 링크 파일이 존재함 >> "%TMP1%"
 
-Write-BAR
+:: IIS 웹 서비스 경로 설정 (실제 경로에 맞게 조정하세요)
+set "IIS_WEB_SERVICE_PATH=C:\inetpub\wwwroot"
 
-@"
-[양호]: 불필요한 웹 서비스가 실행되지 않고 있는 경우
-[취약]: 불필요한 웹 서비스가 실행되고 있는 경우
-"@ | Out-File -FilePath $TMP1 -Append
+:: IIS 웹 서비스 경로 내 심볼릭 링크(.lnk 및 기타 링크 파일) 검색
+echo %IIS_WEB_SERVICE_PATH% 내 심볼릭 링크 파일 검사 중: >> "%TMP1%"
+dir "%IIS_WEB_SERVICE_PATH%\*.lnk" /s /b >> "%TMP1%"
+dir "%IIS_WEB_SERVICE_PATH%\*.url" /s /b >> "%TMP1%"
 
-Write-BAR
+:: 관리자에게의 주의사항
+echo 주의: 이 스크립트는 .lnk 및 .url 바로가기 파일을 나열합니다. 수동으로 검토하여 필요한 파일인지 확인해 주세요. >> "%TMP1%"
 
-# IIS에서 실행 중인 모든 웹 사이트 나열
-$websites = Get-Website
+:: 결과 표시
+type "%TMP1%"
 
-# IIS에서 실행 중인 모든 웹 애플리케이션 나열
-$webApplications = Get-WebApplication
-
-# 웹 사이트 및 웹 애플리케이션 검토
-if ($websites.Count -eq 0 -and $webApplications.Count -eq 0) {
-    Write-Result "OK: 실행 중인 웹 서비스가 없습니다. 결과: 양호(Good)"
-} else {
-    Write-Result "WARN: 다음 웹 서비스가 실행 중입니다. 필요에 따라 검토 및 조치가 필요할 수 있습니다."
-    foreach ($site in $websites) {
-        Write-Result "웹 사이트: $($site.Name)"
-    }
-    foreach ($app in $webApplications) {
-        Write-Result "웹 애플리케이션: $($app.ApplicationPool)"
-    }
-}
-
-Get-Content $TMP1 | Write-Output
+echo.
+echo 스크립트 완료.
