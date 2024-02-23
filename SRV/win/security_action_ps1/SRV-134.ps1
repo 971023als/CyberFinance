b217038ba255877c5b91c4c1fc68bff602f4a845
971023als <971023als@naver.com>
2024-02-24 02:21:46 +0900

    55

--- SRV/win/security_action_ps1/SRV-134.ps1
+++ SRV/win/security_action_ps1/SRV-134.ps1
@@ -1,18 +1,33 @@
-# 결과 파일 정의
-$TMP1 = "$(Get-Date -Format 'yyyyMMddHHmmss')_DEPStatus.log"
-New-Item -Path $TMP1 -ItemType File
+@echo off
+setlocal
 
-# 스택 영역 실행 방지 설정 확인
-$depStatus = Get-WmiObject -Class Win32_OperatingSystem | Select-Object -ExpandProperty DataExecutionPrevention_SupportPolicy
+set TMP1=%SCRIPTNAME%.log
+type NUL > %TMP1%
 
-# DEP 상태에 따른 결과 출력
-switch ($depStatus) {
-    0 {"WARN: DEP가 모든 프로세스에 대해 비활성화되어 있습니다." | Out-File -FilePath $TMP1}
-    1 {"OK: DEP가 필수 Windows 프로그램과 서비스에 대해서만 활성화되어 있습니다." | Out-File -FilePath $TMP1}
-    2 {"WARN: DEP가 모든 프로세스에 대해 활성화되어 있지만, 사용자에 의해 예외가 설정될 수 있습니다." | Out-File -FilePath $TMP1}
-    3 {"OK: DEP가 모든 프로세스에 대해 활성화되어 있습니다." | Out-File -FilePath $TMP1}
-    default {"INFO: DEP 상태를 확인할 수 없습니다." | Out-File -FilePath $TMP1}
-}
+echo ---------------------------------------- >> %TMP1%
+echo CODE [SRV-134] 스택 영역 실행 방지 미설정 >> %TMP1%
+echo ---------------------------------------- >> %TMP1%
 
-# 결과 파일 출력
-Get-Content -Path $TMP1
+echo [양호]: 스택 영역 실행 방지가 활성화된 경우 >> %TMP1%
+echo [취약]: 스택 영역 실행 방지가 비활성화된 경우 >> %TMP1%
+
+echo ---------------------------------------- >> %TMP1%
+
+:: PowerShell을 사용하여 DEP 및 ASLR 설정 확인
+powershell -Command "& {
+    $depStatus = Get-ProcessMitigation -System | Select-Object -ExpandProperty Dep
+    $aslrStatus = Get-ProcessMitigation -System | Select-Object -ExpandProperty Aslr
+
+    if ($depStatus.Enable -eq 'ON' -and $aslrStatus.ForceRelocateImages -eq 'ON') {
+        echo 'OK: 스택 영역 실행 방지가 활성화되어 있습니다.' >> '%TMP1%'
+    } else {
+        echo 'WARN: 스택 영역 실행 방지가 비활성화되어 있습니다.' >> '%TMP1%'
+    }
+}" >> %TMP1%
+
+type %TMP1%
+
+echo.
+echo.
+
+endlocal
