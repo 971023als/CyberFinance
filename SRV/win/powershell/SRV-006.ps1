a6d1e0094642d450f30b99d5629041848ee2baba
971023als <971023als@naver.com>
2024-02-24 01:51:38 +0900

    Update SRV-006.ps1

--- SRV/win/powershell/SRV-006.ps1
+++ SRV/win/powershell/SRV-006.ps1
@@ -1,33 +1,29 @@
-﻿# 필요한 함수 로드 (PowerShell 스크립트로 가정)
-. .\function.ps1
+﻿# 결과 파일 정의
+$TMP1 = "{0}.log" -f $MyInvocation.MyCommand.Name
+# 결과 파일 초기화
+"" | Set-Content $TMP1
 
-# 임시 로그 파일 생성
-$TMP1 = "$([IO.Path]::GetFileNameWithoutExtension($MyInvocation.MyCommand.Name)).log"
-"" | Out-File -FilePath $TMP1
-
-BAR
-
-CODE "SRV-006 SMTP 서비스 로그 수준 설정 미흡"
-
+# 로그 파일에 정보 추가
 @"
+------------------------------------------------
+CODE [SRV-006] SMTP 서비스 로그 수준 설정 미흡
+------------------------------------------------
 [양호]: SMTP 서비스의 로그 수준이 적절하게 설정되어 있는 경우
 [취약]: SMTP 서비스의 로그 수준이 낮거나, 로그가 충분히 수집되지 않는 경우
+------------------------------------------------
 "@ | Out-File -FilePath $TMP1 -Append
 
-BAR
-"[SRV-006] SMTP 서비스 로그 수준 설정 미흡" | Out-File -FilePath $TMP1 -Append
+# SMTP 로그 설정 확인
+# 이 부분은 환경에 따라 적절한 PowerShell cmdlet으로 대체해야 함
+$logLevel = Get-TransportServer | Select-Object -ExpandProperty LogLevel
 
-# SMTP 서비스 로그 수준 설정 확인
-# PowerShell을 사용해 IIS SMTP 로그 설정 확인 (예시로 구현)
-$smtpLogSetting = Get-WebConfigurationProperty -pspath 'MACHINE/WEBROOT/APPHOST' -filter "system.applicationHost/sites/siteDefaults/logFile" -name "logFormat"
-
-if ($smtpLogSetting.Value -eq "W3C") {
-    OK "SMTP 서비스의 로그 수준이 적절하게 설정됨 (현재 설정: W3C)." | Out-File -FilePath $TMP1 -Append
+if ($logLevel -eq 'Medium' -or $logLevel -eq 'High') {
+    "SMTP 서비스의 로그 수준이 적절하게 설정됨." | Out-File -FilePath $TMP1 -Append
 } else {
-    WARN "SMTP 서비스의 로그 수준이 낮게 설정됨 (현재 설정: $($smtpLogSetting.Value))." | Out-File -FilePath $TMP1 -Append
+    "SMTP 서비스의 로그 수준이 낮게 설정됨 또는 설정이 확인되지 않음." | Out-File -FilePath $TMP1 -Append
 }
 
-BAR
+"------------------------------------------------" | Out-File -FilePath $TMP1 -Append
 
-Get-Content $TMP1
-Write-Host `n
+# 결과 파일 출력
+Get-Content $TMP1 | Write-Output
