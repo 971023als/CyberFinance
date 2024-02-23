9461098ec9d486e4ef6cecb45b9f108e4899459b
971023als <971023als@naver.com>
2024-02-23 13:18:42 +0900

    Update SRV-125.ps1

--- SRV/win/change_ps1/SRV-125.ps1
+++ SRV/win/change_ps1/SRV-125.ps1
@@ -1,5 +1,5 @@
 # 결과 파일 정의
-$TMP1 = "$(SCRIPTNAME).log"
+$TMP1 = "$(Get-Location)\$(SCRIPTNAME)_log.txt"
 Remove-Item -Path $TMP1 -ErrorAction Ignore
 New-Item -Path $TMP1 -ItemType File
 
@@ -12,12 +12,15 @@ CODE [SRV-125] 화면보호기 미설정
 "@ | Out-File -FilePath $TMP1
 
 # 화면보호기 설정 확인
-$screenSaverStatus = Get-ItemProperty -Path 'HKCU:\Control Panel\Desktop' -Name ScreenSaveActive
-
-if ($screenSaverStatus.ScreenSaveActive -eq "1") {
-    "OK: 화면보호기가 설정되어 있습니다." | Out-File -FilePath $TMP1 -Append
-} else {
-    "WARN: 화면보호기가 설정되어 있지 않습니다." | Out-File -FilePath $TMP1 -Append
+try {
+    $screenSaverStatus = Get-ItemProperty -Path 'HKCU:\Control Panel\Desktop' -Name ScreenSaveActive
+    if ($screenSaverStatus.ScreenSaveActive -eq "1") {
+        "OK: 화면보호기가 설정되어 있습니다." | Out-File -FilePath $TMP1 -Append
+    } else {
+        "WARN: 화면보호기가 설정되어 있지 않습니다." | Out-File -FilePath $TMP1 -Append
+    }
+} catch {
+    "ERROR: 화면보호기 설정 상태를 확인하는 중 오류가 발생했습니다." | Out-File -FilePath $TMP1 -Append
 }
 
 # 결과 파일 출력
