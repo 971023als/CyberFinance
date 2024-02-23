03aef55561ccdecedfd52be6325c38e3fb630ecf
971023als <971023als@naver.com>
2024-02-24 02:13:37 +0900

    42

--- SRV/win/security_action_ps1/SRV-042.ps1
+++ SRV/win/security_action_ps1/SRV-042.ps1
@@ -1,33 +1,38 @@
-@echo off
-setlocal enabledelayedexpansion
+# 임시 로그 파일 생성
+$TMP1 = "$([System.IO.Path]::GetFileNameWithoutExtension($MyInvocation.MyCommand.Name)).log"
+"" | Out-File -FilePath $TMP1
 
-set "TMP1=%~n0.log"
-type nul > !TMP1!
+# 메시지 구분자 함수
+function BAR {
+    "------------------------------------------------" | Out-File -FilePath $TMP1 -Append
+}
 
-echo ------------------------------------------------ >> !TMP1!
-echo CODE [SRV-042] 웹 서비스 상위 디렉터리 접근 제한 설정 미흡 >> !TMP1!
-echo ------------------------------------------------ >> !TMP1!
+BAR
+"CODE [SRV-042] 웹 서비스 상위 디렉터리 접근 제한 설정 미흡" | Out-File -FilePath $TMP1 -Append
 
-echo [양호]: DocumentRoot가 별도의 보안 디렉터리로 지정된 경우 >> !TMP1!
-echo [취약]: DocumentRoot가 기본 디렉터리 또는 민감한 디렉터리로 지정된 경우 >> !TMP1!
-echo ------------------------------------------------ >> !TMP1!
+@"
+[양호]: DocumentRoot가 별도의 보안 디렉터리로 지정된 경우
+[취약]: DocumentRoot가 기본 디렉터리 또는 민감한 디렉터리로 지정된 경우
+"@ | Out-File -FilePath $TMP1 -Append
 
-:: 디렉터리 접근 권한 설정 확인 (PowerShell 사용)
-powershell -Command "& {
-    Import-Module WebAdministration;
-    $sites = Get-Website | foreach { $_.Name };
-    foreach ($site in $sites) {
-        $rootPath = Get-WebApplication -Site $site | Select -ExpandProperty PhysicalPath;
-        $accessPolicy = Get-WebConfigurationProperty -pspath 'IIS:\Sites\'$site -filter 'system.webServer/security/authorization' -name '.Collection';
-        if ($accessPolicy.Count -eq 0) {
-            Add-Content !TMP1! ('WARN: 웹 사이트 '''+$site+'''에서 상위 디렉터리 접근 제한 설정이 미흡합니다. 루트 경로: '+$rootPath);
-        } else {
-            Add-Content !TMP1! ('OK: 웹 사이트 '''+$site+'''에서 상위 디렉터리 접근 제한 설정이 적절합니다. 루트 경로: '+$rootPath);
-        }
+BAR
+
+# 디렉터리 접근 권한 설정 확인
+Import-Module WebAdministration
+$sites = Get-Website
+
+foreach ($site in $sites) {
+    $rootPath = Get-WebApplication -Site $site.Name | Select -ExpandProperty PhysicalPath
+    $accessPolicy = Get-WebConfigurationProperty -pspath "IIS:\Sites\$($site.Name)" -filter "system.webServer/security/authorization" -name ".Collection"
+    
+    if ($accessPolicy.Count -eq 0) {
+        "WARN: 웹 사이트 '$($site.Name)'에서 상위 디렉터리 접근 제한 설정이 미흡합니다. 루트 경로: $rootPath" | Out-File -FilePath $TMP1 -Append
+    } else {
+        "OK: 웹 사이트 '$($site.Name)'에서 상위 디렉터리 접근 제한 설정이 적절합니다. 루트 경로: $rootPath" | Out-File -FilePath $TMP1 -Append
     }
-}"
+}
 
-echo ------------------------------------------------ >> !TMP1!
-type !TMP1!
+BAR
 
-echo.
+# 결과 출력
+Get-Content -Path $TMP1 | Write-Host
