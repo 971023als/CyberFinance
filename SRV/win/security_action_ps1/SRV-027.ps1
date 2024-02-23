2ce6e6ef751bc2c58abd982c581fb5a19a1305e3
971023als <971023als@naver.com>
2024-02-24 02:01:34 +0900

    Update SRV-027.ps1

--- SRV/win/security_action_ps1/SRV-027.ps1
+++ SRV/win/security_action_ps1/SRV-027.ps1
@@ -1,31 +1,33 @@
-﻿@echo off
-setlocal enabledelayedexpansion
+﻿# 임시 로그 파일 생성
+$TMP1 = "$([System.IO.Path]::GetFileNameWithoutExtension($MyInvocation.MyCommand.Name)).log"
+"" | Out-File -FilePath $TMP1
 
-set "TMP1=%~n0.log"
-type nul > !TMP1!
+# 메시지 구분자
+function BAR {
+    "------------------------------------------------" | Out-File -FilePath $TMP1 -Append
+}
 
-echo ------------------------------------------------ >> !TMP1!
-echo CODE [SRV-027] 서비스 접근 IP 및 포트 제한 미비 >> !TMP1!
-echo ------------------------------------------------ >> !TMP1!
+BAR
+"CODE [SRV-027] 서비스 접근 IP 및 포트 제한 미비" | Out-File -FilePath $TMP1 -Append
+@"
+[양호]: 서비스에 대한 IP 및 포트 접근 제한이 적절하게 설정된 경우
+[취약]: 서비스에 대한 IP 및 포트 접근 제한이 설정되지 않은 경우
+"@ | Out-File -FilePath $TMP1 -Append
 
-echo [양호]: 서비스에 대한 IP 및 포트 접근 제한이 적절하게 설정된 경우 >> !TMP1!
-echo [취약]: 서비스에 대한 IP 및 포트 접근 제한이 설정되지 않은 경우 >> !TMP1!
-echo ------------------------------------------------ >> !TMP1!
+BAR
 
-:: 방화벽 규칙 확인 (PowerShell 사용)
-powershell -Command "& {
-    $FirewallRules = Get-NetFirewallRule -Enabled True -Direction Inbound | Where-Object { $_.Action -eq 'Block' };
-    if ($FirewallRules.Count -gt 0) {
-        foreach ($rule in $FirewallRules) {
-            $ruleName = $rule.DisplayName;
-            Add-Content !TMP1! ('OK: 방화벽 규칙 '''+ $ruleName +'''가 접근을 차단하도록 설정되어 있습니다.');
-        }
-    } else {
-        Add-Content !TMP1! 'WARN: 서비스에 대한 IP 및 포트 접근 제한을 설정하는 방화벽 규칙이 없습니다.';
+# 방화벽 규칙 확인
+$FirewallRules = Get-NetFirewallRule -Enabled True -Direction Inbound | Where-Object { $_.Action -eq 'Block' }
+if ($FirewallRules.Count -gt 0) {
+    foreach ($rule in $FirewallRules) {
+        $ruleName = $rule.DisplayName
+        "OK: 방화벽 규칙 '$ruleName'가 접근을 차단하도록 설정되어 있습니다." | Out-File -FilePath $TMP1 -Append
     }
-}"
+} else {
+    "WARN: 서비스에 대한 IP 및 포트 접근 제한을 설정하는 방화벽 규칙이 없습니다." | Out-File -FilePath $TMP1 -Append
+}
 
-echo ------------------------------------------------ >> !TMP1!
-type !TMP1!
+BAR
 
-echo.
+# 결과 출력
+Get-Content $TMP1 | Write-Host
