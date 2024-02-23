a34cc25a3bd8dd8d8c9647ed97f0ce46e31cba10
971023als <971023als@naver.com>
2024-02-24 02:08:02 +0900

    Update SRV-035.ps1

--- SRV/win/powershell/SRV-035.ps1
+++ SRV/win/powershell/SRV-035.ps1
@@ -1,61 +1,36 @@
-# 결과 파일 초기화
-$TMP1 = "$(Get-Location)\SRV-035_log.txt"
-"" | Set-Content $TMP1
+# 임시 로그 파일 생성
+$TMP1 = "$([System.IO.Path]::GetFileNameWithoutExtension($MyInvocation.MyCommand.Name)).log"
+"" | Out-File -FilePath $TMP1
 
-Function Write-BAR {
-    "-------------------------------------------------" | Out-File -FilePath $TMP1 -Append
+# 메시지 구분자 함수
+function BAR {
+    "------------------------------------------------" | Out-File -FilePath $TMP1 -Append
 }
 
-Function Write-OK {
-    Param ([string]$message)
-    "$message" | Out-File -FilePath $TMP1 -Append
-}
-
-Function Write-WARN {
-    Param ([string]$message)
-    "WARN: $message" | Out-File -FilePath $TMP1 -Append
-}
-
-Write-BAR
+BAR
+"CODE [SRV-035] 취약한 서비스 활성화" | Out-File -FilePath $TMP1 -Append
 
 @"
 [양호]: 취약한 서비스가 비활성화된 경우
 [취약]: 취약한 서비스가 활성화된 경우
 "@ | Out-File -FilePath $TMP1 -Append
 
-Write-BAR
+BAR
 
 # 취약한 서비스 목록
-$services = @("echo", "discard", "daytime", "chargen")
+$services = @('Telnet', 'RemoteRegistry')
 
-# /etc/xinetd.d 폴더 내의 서비스 파일들을 확인합니다.
-if (Test-Path -Path "/etc/xinetd.d") {
-    foreach ($service in $services) {
-        $filePath = "/etc/xinetd.d/$service"
-        if (Test-Path -Path $filePath) {
-            $content = Get-Content -Path $filePath
-            $disabled = $content | Where-Object { $_ -match "disable\s*=\s*yes" }
-            if (-not $disabled) {
-                Write-WARN "$service 서비스가 /etc/xinetd.d 디렉터리 내 서비스 파일에서 실행 중입니다."
-                exit
-            }
-        }
+# 서비스 상태 확인
+foreach ($service in $services) {
+    $serviceStatus = Get-Service -Name $service -ErrorAction SilentlyContinue
+    if ($serviceStatus -and $serviceStatus.Status -eq 'Running') {
+        "WARN: 취약한 서비스 '$service'가 활성화되어 있습니다." | Out-File -FilePath $TMP1 -Append
+    } else {
+        "OK: 서비스 '$service'가 비활성화되어 있거나, 시스템에 설치되지 않았습니다." | Out-File -FilePath $TMP1 -Append
     }
 }
 
-# /etc/inetd.conf 파일에서 서비스들을 확인합니다.
-if (Test-Path -Path "/etc/inetd.conf") {
-    $inetdConfContent = Get-Content -Path "/etc/inetd.conf"
-    foreach ($service in $services) {
-        $enabled = $inetdConfContent | Where-Object { $_ -match $service -and -not $_.StartsWith('#') }
-        if ($enabled) {
-            Write-WARN "$service 서비스가 /etc/inetd.conf 파일에서 실행 중입니다."
-            exit
-        }
-    }
-}
+BAR
 
-Write-OK "※ U-23 결과 : 양호(Good)"
-
-# 최종 결과를 출력합니다.
-Get-Content $TMP1 | Write-Output
+# 결과 출력
+Get-Content -Path $TMP1 | Write-Host
