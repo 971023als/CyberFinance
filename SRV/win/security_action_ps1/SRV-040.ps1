a0653b771b9f9f125afb7f576868b7aa254fcb61
971023als <971023als@naver.com>
2024-02-24 02:11:46 +0900

    4041

--- SRV/win/security_action_ps1/SRV-040.ps1
+++ SRV/win/security_action_ps1/SRV-040.ps1
@@ -1,32 +1,35 @@
-@echo off
-setlocal enabledelayedexpansion
+# 임시 로그 파일 생성
+$TMP1 = "$([System.IO.Path]::GetFileNameWithoutExtension($MyInvocation.MyCommand.Name)).log"
+"" | Out-File -FilePath $TMP1
 
-set "TMP1=%~n0.log"
-type nul > !TMP1!
+# 메시지 구분자 함수
+function BAR {
+    "------------------------------------------------" | Out-File -FilePath $TMP1 -Append
+}
 
-echo ------------------------------------------------ >> !TMP1!
-echo CODE [SRV-040] 웹 서비스 디렉터리 리스팅 방지 설정 미흡 >> !TMP1!
-echo ------------------------------------------------ >> !TMP1!
+BAR
+"CODE [SRV-040] 웹 서비스 디렉터리 리스팅 방지 설정 미흡" | Out-File -FilePath $TMP1 -Append
 
-echo [양호]: 웹 서비스 디렉터리 리스팅이 적절하게 방지된 경우 >> !TMP1!
-echo [취약]: 웹 서비스 디렉터리 리스팅 방지 설정이 미흡한 경우 >> !TMP1!
-echo ------------------------------------------------ >> !TMP1!
+@"
+[양호]: 웹 서비스 디렉터리 리스팅이 적절하게 방지된 경우
+[취약]: 웹 서비스 디렉터리 리스팅 방지 설정이 미흡한 경우
+"@ | Out-File -FilePath $TMP1 -Append
 
-:: 디렉터리 브라우징 설정 확인 (PowerShell 사용)
-powershell -Command "& {
-    Import-Module WebAdministration;
-    $sites = Get-Website;
-    foreach ($site in $sites) {
-        $directoryBrowsing = Get-WebConfigurationProperty -pspath 'IIS:\Sites\$($site.name)' -filter 'system.webServer/directoryBrowse' -name 'enabled';
-        if ($directoryBrowsing.Value -eq $true) {
-            Add-Content !TMP1! ('WARN: 웹 사이트 ''$($site.name)''에서 디렉터리 브라우징이 활성화되어 있습니다.');
-        } else {
-            Add-Content !TMP1! ('OK: 웹 사이트 ''$($site.name)''에서 디렉터리 브라우징이 비활성화되어 있습니다.');
-        }
+BAR
+
+# 디렉터리 브라우징 설정 확인
+Import-Module WebAdministration
+$sites = Get-Website
+foreach ($site in $sites) {
+    $directoryBrowsing = Get-WebConfigurationProperty -pspath "IIS:\Sites\$($site.name)" -filter 'system.webServer/directoryBrowse' -name 'enabled'
+    if ($directoryBrowsing.Value -eq $true) {
+        "WARN: 웹 사이트 '$($site.name)'에서 디렉터리 브라우징이 활성화되어 있습니다." | Out-File -FilePath $TMP1 -Append
+    } else {
+        "OK: 웹 사이트 '$($site.name)'에서 디렉터리 브라우징이 비활성화되어 있습니다." | Out-File -FilePath $TMP1 -Append
     }
-}"
+}
 
-echo ------------------------------------------------ >> !TMP1!
-type !TMP1!
+BAR
 
-echo.
+# 결과 출력
+Get-Content -Path $TMP1 | Write-Host
--- SRV/win/security_action_ps1/SRV-041.ps1
+++ SRV/win/security_action_ps1/SRV-041.ps1
@@ -1,32 +1,36 @@
-@echo off
-setlocal enabledelayedexpansion
+# 임시 로그 파일 생성
+$TMP1 = "$([System.IO.Path]::GetFileNameWithoutExtension($MyInvocation.MyCommand.Name)).log"
+"" | Out-File -FilePath $TMP1
 
-set "TMP1=%~n0.log"
-type nul > !TMP1!
+# 메시지 구분자 함수
+function BAR {
+    "------------------------------------------------" | Out-File -FilePath $TMP1 -Append
+}
 
-echo ------------------------------------------------ >> !TMP1!
-echo CODE [SRV-041] 웹 서비스의 CGI 스크립트 관리 미흡 >> !TMP1!
-echo ------------------------------------------------ >> !TMP1!
+BAR
+"CODE [SRV-041] 웹 서비스의 CGI 스크립트 관리 미흡" | Out-File -FilePath $TMP1 -Append
 
-echo [양호]: CGI 스크립트 관리가 적절하게 설정된 경우 >> !TMP1!
-echo [취약]: CGI 스크립트 관리가 미흡한 경우 >> !TMP1!
-echo ------------------------------------------------ >> !TMP1!
+@"
+[양호]: CGI 스크립트 관리가 적절하게 설정된 경우
+[취약]: CGI 스크립트 관리가 미흡한 경우
+"@ | Out-File -FilePath $TMP1 -Append
 
-:: CGI 스크립트 실행 설정 확인 (PowerShell 사용)
-powershell -Command "& {
-    Import-Module WebAdministration;
-    $cgiSettings = Get-WebConfiguration -Filter '/system.webServer/handlers' -PSPath 'IIS:\';
-    $cgiHandlers = $cgiSettings.Collection | Where-Object { $_.path -eq '*.cgi' -or $_.path -eq '*.pl' };
-    if ($cgiHandlers) {
-        foreach ($handler in $cgiHandlers) {
-            Add-Content !TMP1! ('WARN: CGI 스크립트 (' + $handler.path + ') 실행이 허용되어 있습니다.');
-        }
-    } else {
-        Add-Content !TMP1! 'OK: CGI 스크립트 실행이 적절하게 제한되어 있습니다.';
+BAR
+
+# CGI 스크립트 실행 설정 확인
+Import-Module WebAdministration
+$cgiSettings = Get-WebConfiguration -Filter '/system.webServer/handlers' -PSPath 'IIS:\'
+$cgiHandlers = $cgiSettings.Collection | Where-Object { $_.path -eq '*.cgi' -or $_.path -eq '*.pl' }
+
+if ($cgiHandlers) {
+    foreach ($handler in $cgiHandlers) {
+        "WARN: CGI 스크립트 ($($handler.path)) 실행이 허용되어 있습니다." | Out-File -FilePath $TMP1 -Append
     }
-}"
+} else {
+    "OK: CGI 스크립트 실행이 적절하게 제한되어 있습니다." | Out-File -FilePath $TMP1 -Append
+}
 
-echo ------------------------------------------------ >> !TMP1!
-type !TMP1!
+BAR
 
-echo.
+# 결과 출력
+Get-Content -Path $TMP1 | Write-Host
