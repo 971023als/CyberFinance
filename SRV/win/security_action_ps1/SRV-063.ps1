66360a5fb015e0f78c157b0c6b46212f7334e6e7
971023als <971023als@naver.com>
2024-02-24 14:46:44 +0900

    Update SRV-063.ps1

--- SRV/win/security_action_ps1/SRV-063.ps1
+++ SRV/win/security_action_ps1/SRV-063.ps1
@@ -7,28 +7,25 @@ Clear-Content -Path $TMP1
 
 BAR
 
-$CODE = "[SRV-063] DNS Recursive Query 설정 미흡"
-
-$result = $TMP1
-# 결과 파일에 내용 추가
-Add-Content -Path $result -Value "[양호]: DNS 서버에서 재귀적 쿼리가 제한적으로 설정된 경우"
-Add-Content -Path $result -Value "[취약]: DNS 서버에서 재귀적 쿼리가 적절하게 제한되지 않은 경우"
+$CODE = "[SRV-063] DNS Recursive Query 설정 조치"
+Add-Content -Path $TMP1 -Value $CODE
 
 BAR
 
-# Windows DNS 서버에서 재귀적 쿼리 설정 확인
+# Windows DNS 서버에서 재귀적 쿼리 설정 변경
 Import-Module DNSServer
 
-# 재귀 설정 확인
+# 재귀 설정 변경
+Set-DnsServerSetting -RecursionScope "OnlySecure" -Confirm:$false
+
+# 변경 후 설정 확인
 $recursionSetting = Get-DnsServerSetting | Select-Object -ExpandProperty RecursionScope
 
 Switch ($recursionSetting) {
     "OnlySecure" { OK "DNS 서버에서 재귀적 쿼리가 안전하게 제한됨: $recursionSetting" }
-    "OnlyThisServer" { OK "DNS 서버에서 재귀적 쿼리가 이 서버에만 제한됨: $recursionSetting" }
-    "NoRecursion" { OK "DNS 서버에서 재귀적 쿼리가 비활성화됨: $recursionSetting" }
-    Default { WARN "DNS 서버에서 재귀적 쿼리 제한이 미흡함: $recursionSetting" }
+    Default { WARN "DNS 서버에서 재귀적 쿼리 설정 변경 시도 실패: $recursionSetting" }
 }
 
-Get-Content -Path $result | Out-Host
+Get-Content -Path $TMP1 | Out-Host
 
-Write-Host "`n"
+Write-Host "`n스크립트 완료. DNS 서버의 재귀적 쿼리 설정이 변경되었습니다."
