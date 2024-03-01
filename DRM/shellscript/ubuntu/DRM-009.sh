7790875046ef193efa2b2e2e04eb7893df94634b
971023als <971023als@naver.com>
2024-03-01 21:23:38 +0900

    Update DRM-009.sh

--- DRM/shellscript/ubuntu/DRM-009.sh
+++ DRM/shellscript/ubuntu/DRM-009.sh
@@ -15,13 +15,36 @@ EOF
 
 BAR
 
-read -p "Enter MySQL username: " MYSQL_USER
-read -sp "Enter MySQL password: " MYSQL_PASS
-echo
+echo "지원하는 데이터베이스: MySQL, PostgreSQL"
+read -p "사용 중인 데이터베이스 유형을 입력하세요: " DB_TYPE
 
-MYSQL_CMD="mysql -u $MYSQL_USER -p$MYSQL_PASS -Bse"
+case $DB_TYPE in
+    MySQL|mysql)
+        read -p "Enter MySQL username: " MYSQL_USER
+        read -sp "Enter MySQL password: " MYSQL_PASS
+        echo
+        MYSQL_CMD="mysql -u $MYSQL_USER -p$MYSQL_PASS -Bse"
+        QUERY="SHOW VARIABLES LIKE 'wait_timeout';"
+        ;;
+    PostgreSQL|postgresql)
+        read -p "Enter PostgreSQL username: " PGSQL_USER
+        read -sp "Enter PostgreSQL password: " PGSQL_PASS
+        echo
+        PGSQL_CMD="psql -U $PGSQL_USER -c"
+        QUERY="SHOW idle_in_transaction_session_timeout;"
+        ;;
+    *)
+        echo "Unsupported database type."
+        rm $TMP1
+        exit 1
+        ;;
+esac
 
-SESSION_TIMEOUT=$($MYSQL_CMD "SHOW VARIABLES LIKE 'wait_timeout';")
+if [[ $DB_TYPE == "MySQL" || $DB_TYPE == "mysql" ]]; then
+    SESSION_TIMEOUT=$($MYSQL_CMD "$QUERY")
+elif [[ $DB_TYPE == "PostgreSQL" || $DB_TYPE == "postgresql" ]]; then
+    SESSION_TIMEOUT=$($PGSQL_CMD "$QUERY")
+fi
 
 if [[ -z "$SESSION_TIMEOUT" ]]; then
     WARN "세션 종료 시간이 설정되어 있지 않습니다."
