6439a9b51c9e39d7511fe77c9d93cc06f5b340ce
971023als <971023als@naver.com>
2024-03-02 23:01:59 +0900

    16 17

--- DRM/bat/ubuntu/DRM-016.sh
+++ DRM/bat/ubuntu/DRM-016.sh
@@ -8,7 +8,7 @@ echo [양호]: 최신 보안 패치와 벤더 권고사항이 적용된 경우
 echo [취약]: 최신 보안 패치와 벤더 권고사항이 적용되지 않은 경우
 echo ============================================
 
-echo 지원하는 데이터베이스: MySQL, PostgreSQL, Oracle
+echo 지원하는 데이터베이스: MySQL, PostgreSQL, Oracle, MSSQL
 set /p DB_TYPE="사용 중인 데이터베이스 유형을 입력하세요: "
 
 if "%DB_TYPE%"=="MySQL" (
@@ -20,6 +20,10 @@ if "%DB_TYPE%"=="MySQL" (
 ) else if "%DB_TYPE%"=="Oracle" (
     for /f "tokens=*" %%i in ('sqlplus -v') do set VERSION=%%i
     set "VERSION=%VERSION:*Release =%"
+    set "VERSION=%VERSION:*,=%"
+) else if "%DB_TYPE%"=="MSSQL" (
+    for /f "tokens=*" %%i in ('sqlcmd -Q "SELECT @@VERSION" -h -1') do set VERSION=%%i
+    set "VERSION=%VERSION:*Server =%"
     set "VERSION=%VERSION:*,=%"
 ) else (
     echo 지원되지 않는 데이터베이스 유형입니다.
