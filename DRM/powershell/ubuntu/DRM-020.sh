ac793bca2f51b9de74440fb13de94cea6260502e
971023als <971023als@naver.com>
2024-03-03 01:06:24 +0900

    Update DRM-020.sh

--- DRM/powershell/ubuntu/DRM-020.sh
+++ DRM/powershell/ubuntu/DRM-020.sh
@@ -1,32 +1,43 @@
 # Define function to prompt for secure password
 function Read-SecurePassword {
     $securePassword = Read-Host "Enter the database administrator password" -AsSecureString
-    return [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($securePassword))
+    return $securePassword
 }
 
+# Import SQLServer module
+Import-Module SqlServer
+
 # Prompt for database information
-$dbType = Read-Host "Supported databases: MySQL, PostgreSQL. Enter the type of your database"
+$dbType = Read-Host "Supported databases: 1. MySQL, 2. PostgreSQL, 3. SQL Server. Enter the number corresponding to your database type"
 $dbUser = Read-Host "Enter the database administrator username"
 $dbPass = Read-SecurePassword
 
 # Define and execute database command based on the database type
-if ($dbType -eq "MySQL") {
-    $query = "SELECT User, Host, Db, Select_priv, Insert_priv, Update_priv FROM mysql.db;"
-    $command = "mysql -u $dbUser -p$dbPass -e `"$query`""
-    Invoke-Expression $command
-}
-elseif ($dbType -eq "PostgreSQL") {
-    $query = "SELECT rolname, rolselectpriv, rolinsertpriv, rolupdatepriv FROM pg_roles JOIN pg_database ON (rolname = datname);"
-    $command = "psql -U $dbUser -c `"$query`""
-    Invoke-Expression $command
-}
-else {
-    Write-Host "Unsupported database type."
-    exit
+switch ($dbType) {
+    "1" { # MySQL
+        $query = "SELECT User, Host, Db, Select_priv, Insert_priv, Update_priv FROM mysql.db;"
+        # For MySQL, you might use a .NET connector or call a command-line tool
+        Write-Host "MySQL command execution not implemented in this script. Please use a MySQL client."
+    }
+    "2" { # PostgreSQL
+        $query = "SELECT rolname, rolselectpriv, rolinsertpriv, rolupdatepriv FROM pg_roles JOIN pg_database ON (rolname = datname);"
+        # For PostgreSQL, psql or a .NET connector could be used
+        Write-Host "PostgreSQL command execution not implemented in this script. Please use psql or a .NET connector."
+    }
+    "3" { # SQL Server
+        $secureStringPassword = Read-SecurePassword
+        $plainPassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($secureStringPassword))
+        $sqlCredential = New-Object System.Management.Automation.PSCredential ($dbUser, $secureStringPassword)
+
+        $query = "SELECT name, is_disabled FROM sys.sql_logins"
+        Invoke-Sqlcmd -Query $query -Username $dbUser -Password $plainPassword -ServerInstance "YourServerInstanceName" # Adjust ServerInstance as necessary
+    }
+    default {
+        Write-Host "Unsupported database type."
+        exit
+    }
 }
 
-# Note: Direct execution of database commands in PowerShell using Invoke-Expression with concatenated strings is a security risk similar to SQL injection.
-# It's recommended to use parameterized queries or stored procedures to mitigate this risk.
-
-# The above example demonstrates how to execute database-specific commands in PowerShell.
-# The actual execution and results parsing will require adjustments based on your database setup and security policies.
+# Note: For SQL Server, using Invoke-Sqlcmd with a PSCredential object is a secure way to handle passwords.
+# Ensure to replace "YourServerInstanceName" with your actual SQL Server instance name.
+# This script assumes SQL Server PowerShell module (SqlServer) is installed and available.
