14bbf3922500ce982dc290f5dd6fa66186ca2997
971023als <971023als@naver.com>
2024-03-02 20:17:05 +0900

    Update DRM-032.sh

--- DRM/bat/ubuntu/DRM-032.sh
+++ DRM/bat/ubuntu/DRM-032.sh
@@ -1,5 +1,9 @@
 @echo off
-setlocal
+setlocal enabledelayedexpansion
+
+echo -----------------------------------------------------
+echo CODE [DBM-032] 데이터베이스 접속 시 통신구간에 비밀번호 평문 노출
+echo -----------------------------------------------------
 
 echo 지원하는 데이터베이스:
 echo 1. SQL Server
@@ -11,7 +15,7 @@ set /p DB_ADMIN="데이터베이스 관리자 사용자 이름을 입력하세�
 
 if "%DB_TYPE%"=="1" (
     echo SQL Server 선택됨. 비밀번호는 sqlcmd에 의해 요청될 것입니다.
-    sqlcmd -U %DB_ADMIN% -Q "SELECT name FROM sys.sql_logins WHERE name = 'sa';"
+    sqlcmd -U %DB_ADMIN% -Q "SELECT name FROM sys.sql_logins WHERE name = 'sa';" > nul 2>&1
     if errorlevel 1 (
         echo 관리자 계정 보안 설정 검사 실패.
     ) else (
@@ -19,7 +23,7 @@ if "%DB_TYPE%"=="1" (
     )
 ) else if "%DB_TYPE%"=="2" (
     set /p DB_PASS="MySQL 데이터베이스 관리자 비밀번호를 입력하세요: "
-    mysql -u %DB_ADMIN% -p%DB_PASS% -e "SELECT User, Host FROM mysql.user WHERE User = 'root';"
+    mysql -u %DB_ADMIN% -p%DB_PASS% -e "SELECT User, Host FROM mysql.user WHERE User = 'root';" > nul 2>&1
     if errorlevel 1 (
         echo 관리자 계정 보안 설정 검사 실패.
     ) else (
@@ -27,7 +31,7 @@ if "%DB_TYPE%"=="1" (
     )
 ) else if "%DB_TYPE%"=="3" (
     echo PostgreSQL 선택됨. 비밀번호는 psql에 의해 요청될 것입니다.
-    psql -U %DB_ADMIN% -c "SELECT rolname FROM pg_roles WHERE rolname = 'postgres';"
+    psql -U %DB_ADMIN% -c "SELECT rolname FROM pg_roles WHERE rolname = 'postgres';" > nul 2>&1
     if errorlevel 1 (
         echo 관리자 계정 보안 설정 검사 실패.
     ) else (
